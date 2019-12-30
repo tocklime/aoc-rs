b1b758cbf059ae640a1580275452710b431fd2b2
Greg Manning <1396183+tocklime@users.noreply.github.com>
2019-12-30 11:26:47 +0000

    2015d1

---
+++ 2015/src/lib.rs
@@ -0,0 +1,13 @@
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+pub mod sec1;
+pub mod sec2;
+pub mod sec3;
+pub mod sec4;
+pub mod sec5;
+pub mod utils;
+
+aoc_lib! { year = 2015 }
