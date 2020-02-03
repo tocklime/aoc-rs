e379165be6db02fba980da9cfc78e767d24b31b1
Greg Manning <1396183+tocklime@users.noreply.github.com>
2020-02-03 20:07:50 +0000

    2017 day 1

---
+++ 2017/src/sec1/day01.rs
@@ -0,0 +1,18 @@
+use itertools::Itertools;
+
+#[aoc(day1, part1)]
+fn p1(input: &str) -> u32 {
+    let mut digs = input.chars().map(|x| x.to_digit(10).unwrap()).collect_vec();
+    digs.push(digs[0]);
+    digs.into_iter().tuple_windows()
+        .filter_map(|(a, b)| if a == b { Some(a) } else { None })
+        .sum()
+}
+
+#[aoc(day1, part2)]
+fn p2(input: &str) -> u32 {
+    let mut digs = input.chars().map(|x| x.to_digit(10).unwrap()).collect_vec();
+    let (a, b) = digs.split_at(digs.len() / 2);
+    a.into_iter().zip(b).filter_map(|(a, b)| if a == b { Some(*a) } else { None })
+        .sum::<u32>() * 2
+}
