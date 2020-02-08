0c61086e8feb61ea0987bec3ea3da427cf4cbe32
Greg Manning <1396183+tocklime@users.noreply.github.com>
2020-02-08 15:35:55 +0000

    2017 day 12

---
+++ 2017/src/sec3/day11.rs
@@ -0,0 +1,52 @@
+use num::abs;
+use std::cmp::max;
+
+#[derive(Debug, Copy, Clone)]
+struct Pos { n: i32, ne: i32 }
+
+impl Pos {
+    fn origin() -> Self {
+        Pos { n: 0, ne: 0 }
+    }
+    fn dist(&self) -> i32 {
+        return if (self.n > 0) != (self.ne > 0) {
+            //is nw or se.
+            max(abs(self.n), abs(self.ne))
+        } else {
+            abs(self.n) + abs(self.ne)
+        };
+    }
+    fn step(&mut self, dir: &str) {
+        match dir {
+            "n" => { self.n += 1; }
+            "ne" => { self.ne += 1; }
+            "se" => {
+                self.n -= 1;
+                self.ne += 1;
+            }
+            "s" => { self.n -= 1; }
+            "sw" => { self.ne -= 1; }
+            "nw" => {
+                self.n += 1;
+                self.ne -= 1;
+            }
+            _ => panic!("unknown step: {}", dir)
+        }
+    }
+}
+
+
+#[aoc(day11, part1)]
+fn p1(input: &str) -> i32 {
+    let mut pos = Pos::origin();
+    input.split(',').for_each(|x| pos.step(x));
+    pos.dist()
+}
+
+#[aoc(day11, part2)]
+fn p2(input: &str) -> i32 {
+    input.split(',').scan(Pos::origin(),|state, x| {
+        state.step(x);
+        Some(state.dist())
+    }).max().unwrap()
+}
