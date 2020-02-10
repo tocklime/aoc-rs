f87438a8920c5933e9fa1a0ef4c623ae33997360
Greg Manning <1396183+tocklime@users.noreply.github.com>
2020-02-10 07:21:19 +0000

    2017 end of day 17 and all of day 18

--- 2017/src/sec4/day17.rs
+++ 2017/src/sec4/day17.rs
@@ -16,15 +16,13 @@ fn p1(input: &str) -> usize {
 #[aoc(day17, part2)]
 fn p2(input: &str) -> usize {
     let step = input.trim().parse::<usize>().unwrap();
-    let mut d = VecDeque::<usize>::new();
-    d.push_back(0);
     let mut pos = 0;
+    let mut last_insert_at_1 = 0;
     for n in 1..=50_000_000 {
         pos = ((pos + step) % n) + 1;
-        d.insert(pos, n);
         if pos == 1 {
-            println!("Insert @1: {}", n);
+            last_insert_at_1 = n;
         }
     }
-    d[(pos + 1) % d.len()]
+    last_insert_at_1
 }
