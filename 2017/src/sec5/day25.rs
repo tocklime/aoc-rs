3b21b09e33f0ddd3fbfd5b92ea479cb7e4c6d386
Greg Manning <1396183+tocklime@users.noreply.github.com>
2020-02-20 07:55:16 +0000

    2017 days 24 and 25 :D

---
+++ 2017/src/sec5/day25.rs
@@ -0,0 +1,34 @@
+use std::collections::HashSet;
+
+#[aoc(day25,part1)]
+fn p1(_input: &str) -> usize {
+    let mut state = 'A';
+    let c = 12667664;
+    let mut pos = 0;
+    let mut tape: HashSet<i64> = HashSet::new();
+    for _ in 0..c {
+        let (set,mv,st2) = match (state,tape.contains(&pos)) {
+            ('A',false) => (true,1,'B'),
+            ('A',true) => (false,-1,'C'),
+            ('B',false) => (true,-1,'A'),
+            ('B',true) => (true,1,'D'),
+            ('C',false) => (false,-1,'B'),
+            ('C',true) => (false,-1,'E'),
+            ('D',false) => (true,1,'A'),
+            ('D',true) => (false,1,'B'),
+            ('E',false) => (true,-1,'F'),
+            ('E',true) => (true,-1,'C'),
+            ('F',false) => (true,1,'D'),
+            ('F',true) => (true,1,'A'),
+            (a,_) => panic!("Unknown state {}",a)
+            };
+        if set {
+            tape.insert(pos);
+        } else {
+            tape.remove(&pos);
+        }
+        pos += mv;
+        state = st2;
+    }
+    tape.len()
+}
