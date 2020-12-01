41f98d87739a15355774e16e2b5793f73064fcc5
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-01 05:46:27 +0000

    a more efficient solution with sets

--- 2020/src/sec1/day01.rs
+++ 2020/src/sec1/day01.rs
@@ -1,3 +1,6 @@
+#![warn(clippy::all)]
+use std::collections::HashSet;
+
 use itertools::Itertools;
 #[aoc_generator(day1)]
 pub fn gen(input:&str) -> Vec<i64>{
@@ -13,16 +16,30 @@ pub fn gen(input:&str) -> Vec<i64>{
 pub fn solve(input: &[i64], size: usize) -> i64 {
     input.iter()
         .combinations(size)
-        .filter(|x| x.iter().cloned().sum::<i64>() == 2020)
-        .nth(0)
+        .find(|x| x.iter().cloned().sum::<i64>() == 2020)
         .unwrap()
-        .iter()
-        .cloned()
+        .into_iter()
         .product()
 }
 
-#[aoc(day1,part1)]
+#[aoc(day1,part1,brute)]
 pub fn p1(input: &[i64]) -> i64 { solve(input,2) }
 
-#[aoc(day1,part2)]
+#[aoc(day1,part2,brute)]
 pub fn p2(input: &[i64]) -> i64 { solve(input,3) }
+
+#[aoc(day1,part1,sets)]
+pub fn p1_sets(input: &[i64]) -> i64 { 
+    let s : HashSet<i64> = input.iter().cloned().collect();
+    let n = *s.iter().find(|&&x| s.contains(&(2020 - x))).unwrap();
+    n * (2020 - n)
+}
+
+#[aoc(day1,part2,sets)]
+pub fn p2_sets(input: &[i64]) -> i64 { 
+    let s : HashSet<i64> = input.iter().cloned().collect();
+    let c = s.iter().combinations(2)
+        .map(|x| (x.iter().copied().sum::<i64>(),x))
+        .find(|(sum,_)| s.contains(&(2020 - *sum))).unwrap();
+    c.1.iter().copied().product::<i64>() * (2020 - c.0)
+}
