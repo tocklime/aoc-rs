30f3831745b06583125cc79309142cbaae0fc80a
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-07 11:24:14 +0000

    tidying

--- 2020/src/sec2/day07.rs
+++ 2020/src/sec2/day07.rs
@@ -2,7 +2,6 @@ use crate::utils::collections::ToLookupSet;
 use itertools::Itertools;
 use parse_display::FromStr;
 use std::collections::{HashMap, HashSet};
-
 #[derive(FromStr, Debug, Hash, PartialEq, Eq, Clone)]
 #[from_str(regex = r"(?P<n>[0-9]+) (?P<colour>[a-z ]*) bags?")]
 pub struct Bags {
@@ -10,39 +9,19 @@ pub struct Bags {
     colour: String,
 }
 
-pub fn all_containers<'a>(
-    m: &'a HashMap<&'a str, HashSet<&'a str>>,
-    name: &'a str,
-) -> HashSet<&'a str> {
-    let mut to_explore = vec![name];
-    let mut seen = HashSet::new();
-    while !to_explore.is_empty() {
-        let c = to_explore.pop().unwrap();
-        seen.insert(c);
-        to_explore.extend(m.get(c).unwrap_or(&HashSet::new()) - &seen);
-    }
-    seen
-}
-
-pub fn count_bags(m: &HashMap<&str, HashSet<&Bags>>, name: &str) -> usize {
-    m.get(name).map_or(0, |x| {
-        x.iter().map(|b| b.n * (1 + count_bags(m, &b.colour))).sum()
-    })
-}
 #[aoc_generator(day7)]
 pub fn gen(input: &str) -> Vec<(String, Vec<Bags>)> {
     input
         .lines()
         .map(|l| {
-            let x = l.split(" bags contain ").collect::<Vec<_>>();
-            let n = x[0].to_string();
-            let inners = x[1]
+            let (n, bags) : (&str,&str) = l.split(" bags contain ").next_tuple().unwrap();
+            let inners = bags
                 .trim_end_matches('.')
                 .split(", ")
                 .map(|i| i.parse::<Bags>().ok())
                 .filter_map(|x| x)
-                .collect_vec();
-            (n, inners)
+                .collect();
+            (n.to_string(), inners)
         })
         .collect()
 }
@@ -53,8 +32,22 @@ pub fn p1(input: &[(String, Vec<Bags>)]) -> usize {
         .iter()
         .flat_map(|(n, bags)| bags.iter().map(move |b| (b.colour.as_ref(), n.as_ref())))
         .collect_lookup_set();
-    all_containers(&cs, "shiny gold").len() - 1
+    let mut to_explore = vec!["shiny gold"];
+    let mut seen = HashSet::new();
+    while !to_explore.is_empty() {
+        let c = to_explore.pop().unwrap();
+        seen.insert(c);
+        to_explore.extend(cs.get(c).unwrap_or(&HashSet::new()) - &seen);
+    }
+    seen.len() - 1
 }
+
+pub fn count_bags(m: &HashMap<&str, HashSet<&Bags>>, name: &str) -> usize {
+    m.get(name).map_or(0, |x| {
+        x.iter().map(|b| b.n * (1 + count_bags(m, &b.colour))).sum()
+    })
+}
+
 #[aoc(day7, part2)]
 pub fn p2(input: &[(String, Vec<Bags>)]) -> usize {
     let cs = input
