0dd49e08a1b19800e2b02e1b2367923c7768ad24
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-09 11:59:28 +0000

    i'm done now

--- 2020/src/sec2/day09.rs
+++ 2020/src/sec2/day09.rs
@@ -4,11 +4,12 @@ use crate::utils::collections::{minmax, prefix_sum};
 pub fn gen(input: &str) -> Vec<usize> {
     input.lines().map(|x| x.parse().unwrap()).collect()
 }
+const WIN_SIZE : usize = 25;
 
 #[aoc(day9, part1)]
 pub fn p1(is: &[usize]) -> Option<usize> {
-    is.windows(25).enumerate().find_map(|(ix, w)| {
-        let target = is[25 + ix];
+    is.windows(WIN_SIZE).enumerate().find_map(|(ix, w)| {
+        let target = is[WIN_SIZE + ix];
         if w.iter().all(|x| !w.contains(&(target - x))) {
             Some(target)
         } else {
@@ -44,15 +45,13 @@ pub fn p2_other(is: &[usize]) -> Option<usize> {
     let target = p1(is)?;
     let ps = prefix_sum(is).collect::<Vec<usize>>();
     //for each possible window size
-    (1..=is.len()).find_map(|win_size| {
+    (2..=is.len()).find_map(|win_size| {
         //find a start index such that..
         (0..is.len() - win_size)
-            //(optimisation: give up once the difference is too big)
-            .take_while(|&ix_1| ps[ix_1+win_size] - ps[ix_1] <= target)
             // ..such that the difference on the prefix sum array ==target (that is, is[ix_1..=ix_2].sum() == target)
             .find_map(|ix_1| {
                 if ps[ix_1+win_size] - ps[ix_1] == target {
-                    let (&a, &b) = minmax(&is[ix_1 + 1..=ix_1 + win_size]).unwrap();
+                    let (&a, &b) = minmax(&is[ix_1 + 1..=ix_1 + win_size])?;
                     Some(a + b)
                 } else {
                     None
