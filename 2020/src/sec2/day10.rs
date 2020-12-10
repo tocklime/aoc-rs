cda6047ad530169a6984702fa7c115b0e27b1e62
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-10 07:00:59 +0000

    tidied

--- 2020/src/sec2/day10.rs
+++ 2020/src/sec2/day10.rs
@@ -1,49 +1,55 @@
-use std::collections::{BTreeSet, HashMap};
-
 use itertools::Itertools;
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 
-#[aoc(day10, part1)]
-pub fn p1(input: &str) -> Option<usize> {
-    let mut xs: BTreeSet<usize> = input.lines().map(|x| x.parse::<usize>().unwrap()).collect();
-    let mut one_jumps = 0;
-    let mut three_jumps = 1;
-    xs.insert(0);
-    for (a, b) in xs.iter().tuple_windows() {
-        match b - a {
-            3 => three_jumps += 1,
-            1 => one_jumps += 1,
-            2 => {}
-            _ => {
-                panic!("No");
-            }
-        }
-    }
-    Some(three_jumps * one_jumps)
-}
-
-pub fn count_routes(memo: &mut HashMap<usize,usize>, map: &BTreeSet<usize>, from: usize, to: usize) -> usize {
-    if memo.contains_key(&from) {
-        return memo[&from];
-    }
-    if from == to {
-        return 1;
-    }
-    if from > to {
-        return 0;
-    }
-    let a = (from + 1..=from + 3)
-        .filter(|m| map.contains(m))
-        .map(|m| count_routes(memo, map, m, to))
-        .sum();
-    memo.insert(from,a);
+#[aoc_generator(day10)]
+pub fn gen(input: &str) -> Vec<usize> {
+    let mut a : Vec<usize> = input.lines().map(|x| x.parse::<usize>().unwrap()).collect();
+    a.push(0);
+    a.sort_unstable();
+    a.push(a.last().unwrap() + 3);
     a
 }
 
+#[aoc(day10, part1)]
+pub fn p1(input: &[usize]) -> usize {
+    let (a, b) = input
+        .iter()
+        .tuple_windows()
+        .fold((0, 0), |(a, b), (c,d)| match d - c {
+            1 => (a + 1, b),
+            3 => (a, b + 1),
+            _ => (a, b),
+        });
+    a * b
+}
+
+pub fn count_routes(
+    memo: &mut HashMap<usize, usize>,
+    map: &HashSet<usize>,
+    from: usize,
+    to: usize,
+) -> usize {
+    match (memo.get(&from), from.cmp(&to)) {
+        (Some(&x), _) => x,
+        (_, Ordering::Greater) => 0,
+        (_, Ordering::Equal) => 1,
+        _ => {
+            let a = (from + 1..=from + 3)
+                .filter_map(|m| map.get(&m).map(|&x| count_routes(memo, map, x, to)))
+                .sum();
+            memo.insert(from, a);
+            a
+        }
+    }
+}
+
 #[aoc(day10, part2)]
-pub fn p2(input: &str) -> usize {
-    let mut xs: BTreeSet<usize> = input.lines().map(|x| x.parse::<usize>().unwrap()).collect();
+pub fn p2(input: &[usize]) -> usize {
+    let mut xs: HashSet<usize> = input.iter().copied().collect();
     let target = xs.iter().max().unwrap() + 3_usize;
     xs.insert(target);
-    let mut memo = HashMap::new();
-    count_routes(&mut memo, &xs, 0, target)
+    count_routes(&mut HashMap::new(), &xs, 0, target)
 }
