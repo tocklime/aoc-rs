96d2950fbb2f70782e5a26563f4a10430d7aedb2
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-10 17:02:53 +0000

    faster!

--- 2020/src/sec2/day10.rs
+++ 2020/src/sec2/day10.rs
@@ -1,5 +1,8 @@
 use itertools::Itertools;
-use std::{cmp::Ordering, collections::{HashMap, HashSet, VecDeque}};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet, VecDeque},
+};
 
 #[aoc_generator(day10)]
 pub fn gen(input: &str) -> Vec<usize> {
@@ -59,7 +62,8 @@ pub fn p2_dp(input: &[usize]) -> usize {
     for (ix, val) in input.iter().enumerate() {
         //now at index ix, and there's dp[ix] ways to get here.
         //for each of the next 3 values <= val + 3, add dp[ix] to the ways to get there.
-        for offset in (1..=3).take_while(|&offset| input.get(ix + offset).map_or(false, |&x| x <= val + 3))
+        for offset in
+            (1..=3).take_while(|&offset| input.get(ix + offset).map_or(false, |&x| x <= val + 3))
         {
             dp[ix + offset] += dp[ix];
         }
@@ -68,9 +72,32 @@ pub fn p2_dp(input: &[usize]) -> usize {
     *dp.last().unwrap()
 }
 
+#[aoc(day10, part2, dp_by_value)]
+pub fn p2_dp_by_value(input: &[usize]) -> usize {
+    let mut dp = vec![0; input.last().unwrap() + 4];
+    //the value in dp[ix] is the number of ways to get to ix.
+    dp[0] = 1;
+    input.iter().for_each(|&val| (1..=3).for_each(|x| dp[val + x] += dp[val]));
+    //answer is at the end of dp.
+    *dp.last().unwrap()
+}
+#[aoc(day10, part2, dp_fold)]
+pub fn p2_dp_fold(input: &[usize]) -> usize {
+    input.iter().skip(1).fold((0,1,0,0,0),|(last, a,b,c,d),&x| {
+        match x - last {
+            3 => (x,d+a,0,0,0),
+            1 => (x,b+a,c+a,d+a,0),
+            _ => {
+                dbg!(last,a,b,c,d,x);
+                panic!("nope");
+            }
+        }
+    }).1
+}
+
 #[aoc(day10, part2, dp_deque)]
 pub fn p2_dp_deque(input: &[usize]) -> usize {
-    let mut dp : VecDeque<usize> = [1,0,0,0].iter().copied().collect();
+    let mut dp: VecDeque<usize> = [1, 0, 0, 0].iter().copied().collect();
     //the values in dp are the number of ways to get to the next 3 indexes from here.
     let mut last = 1;
     for (ix, val) in input.iter().enumerate() {
@@ -78,7 +105,8 @@ pub fn p2_dp_deque(input: &[usize]) -> usize {
         //for each of the next 3 values <= val + 3, add dp[ix] to the ways to get there.
         last = dp.pop_front().unwrap();
         dp.push_back(0);
-        for offset in (0..3).take_while(|&offset| input.get(ix + 1 + offset).map_or(false, |&x| x <= val + 3))
+        for offset in
+            (0..3).take_while(|&offset| input.get(ix + 1 + offset).map_or(false, |&x| x <= val + 3))
         {
             dp[offset] += last;
         }
