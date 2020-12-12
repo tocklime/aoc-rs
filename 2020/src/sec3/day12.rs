1546166f582171f98cc863f1cf059fabea2d9e5e
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-12 06:15:31 +0000

    now with folds!

--- 2020/src/sec3/day12.rs
+++ 2020/src/sec3/day12.rs
@@ -9,33 +9,29 @@ pub fn gen(input: &str) -> Vec<(char, isize)> {
 }
 #[aoc(day12, part1)]
 pub fn p1(input: &[(char, isize)]) -> isize {
-    let mut ship: Point<isize> = Point::new(0, 0);
-    let mut dir = Dir::Right;
-    for &l in input {
-        match l {
-            (c, d) if "NSWE".contains(c) => ship += Dir::from_x("NSWE", c).as_point_step() * d,
-            ('F', d) => ship += dir.as_point_step() * d,
-            ('L', 90) | ('R', 270) => dir = dir.turn_left(),
-            ('R', 90) | ('L', 270) => dir = dir.turn_right(),
-            ('L', 180) | ('R', 180) => dir = dir.turn_about(),
+    input
+        .iter()
+        .fold((Point::new(0, 0), Dir::Right), |(ship, dir), &l| match l {
+            (c, d) if "NSWE".contains(c) => (ship + Dir::from_x("NSWE", c).as_point_step() * d, dir),
+            ('F', d) => (ship + dir.as_point_step() * d, dir),
+            ('L', d) => (ship, (0..d/90).fold(dir,|d,_|d.turn_left())),
+            ('R', d) => (ship, (0..d/90).fold(dir,|d,_|d.turn_right())),
             _ => panic!("Unknown instruction {:?}", l),
-        }
-    }
-    ship.manhattan()
+        })
+        .0
+        .manhattan()
 }
 #[aoc(day12, part2)]
 pub fn p2(input: &[(char, isize)]) -> isize {
-    let mut ship: Point<isize> = Point::new(0, 0);
-    let mut waypoint: Point<isize> = Point::new(10, 1);
-    for &l in input {
-        match l {
-            (c, d) if "NWES".contains(c) => waypoint += Dir::from_x("NSWE", c).as_point_step() * d,
-            ('F', d) => ship += waypoint * d,
-            ('L', 90) | ('R', 270) => waypoint = waypoint.rotate_left_about_origin(),
-            ('R', 90) | ('L', 270) => waypoint = waypoint.rotate_right_about_origin(),
-            ('L', 180) | ('R', 180) => waypoint = waypoint.rotate_180_about_origin(),
+    input
+        .iter()
+        .fold((Point::new(0, 0), Point::new(10, 1)), |(ship, way), &l| match l {
+            (c, d) if "NSWE".contains(c) => (ship, way + Dir::from_x("NSWE", c).as_point_step() * d),
+            ('F', d) => (ship + way * d, way),
+            ('L', d) => (ship, (0..d / 90).fold(way, |w, _| w.rotate_left_about_origin())),
+            ('R', d) => (ship, (0..d / 90).fold(way, |w, _| w.rotate_right_about_origin())),
             _ => panic!("Unknown instruction {:?}", l),
-        }
-    }
-    ship.manhattan()
+        })
+        .0
+        .manhattan()
 }
