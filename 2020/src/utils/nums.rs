e84cfd381f2529e32d8f1fdc018911230937cffb
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-13 07:09:01 +0000

    day 13. Modulo arithmetic day.

--- 2020/src/utils/nums.rs
+++ 2020/src/utils/nums.rs
@@ -1,5 +1,6 @@
-use num::{Num, Signed};
-use std::{convert::TryInto, ops::Shr};
+use num::{Integer, Num, Signed};
+use std::{convert::TryInto, iter::{Product, Sum}, ops::Shr};
+use modinverse::modinverse;
 
 pub fn mod_pow<T>(mut base: T, mut exp: T, modulus: T) -> T
     where T: Num + Copy + Shr<Output = T> + From<u8> + PartialOrd
@@ -18,6 +19,7 @@ pub fn mod_pow<T>(mut base: T, mut exp: T, modulus: T) -> T
     }
     result
 }
+
 pub fn add_i<T : Num + Signed + TryInto<usize>>(u : usize, i: &T) -> usize {
     let i_as_u : usize = i.abs().try_into().ok().unwrap();
     if i.is_negative() {
@@ -33,4 +35,11 @@ pub fn add_assign_i<T : Num + Signed + TryInto<usize>>(u : &mut usize, i: &T) {
     } else {
         *u += i_as_u;
     }
+}
+
+pub fn chinese_remainder_theorem<T>(list : &[(T,T)]) -> T 
+    where T : Num + Product + Sum + Integer + Copy,
+{
+    let m_prod: T = list.iter().map(|x| x.1).product();
+    list.iter().map(|&(x,m)| x * (m_prod/m) * modinverse(m_prod/m,m).unwrap()).sum::<T>() % m_prod
 }
