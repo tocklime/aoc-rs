38b9d88dc43a68ad599b2675cacef58dd40c4971
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-14 15:38:47 +0000

    more tidying

--- 2020/src/sec3/day14.rs
+++ 2020/src/sec3/day14.rs
@@ -1,15 +1,13 @@
-use bitvec::{order::Lsb0, prelude::{BitField, BitVec}, view::BitView};
+use bitvec::{
+    order::Lsb0,
+    prelude::{BitField, BitVec},
+    view::BitView,
+};
 use std::{collections::HashMap, num::ParseIntError, str::FromStr};
 
 pub struct Mask {
-    lo : u64,
-    mask: u64
-}
-impl Mask {
-    #[allow(clippy::missing_const_for_fn)]
-    pub fn mask_size(&self) -> u64 {
-        u64::pow(2, self.mask.count_ones())
-    }
+    lo: u64,
+    mask: u64,
 }
 pub enum Line {
     SetMask(Mask),
@@ -20,14 +18,14 @@ impl FromStr for Line {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if let Some(m) = s.strip_prefix("mask = ") {
-            let lo =  m.chars().rev().map(|x| x == '1').collect::<BitVec>().load(); //0,X=> 0, 1=>1
+            let lo = m.chars().rev().map(|x| x == '1').collect::<BitVec>().load(); //0,X=> 0, 1=>1
             let mask = m.chars().rev().map(|x| x == 'X').collect::<BitVec>().load(); //0,1=>0,X=> 1
-            Ok(Self::SetMask(Mask{lo,mask}))
-        }else {
-            let sp :Vec<&str> = s.split(|c| "[] =".contains(c)).collect();
+            Ok(Self::SetMask(Mask { lo, mask }))
+        } else {
+            let sp: Vec<&str> = s.split(|c| "[] =".contains(c)).collect();
             let addr = sp[1].parse()?;
             let val = sp[5].parse()?;
-            Ok(Self::SetMem(addr,val))
+            Ok(Self::SetMem(addr, val))
         }
     }
 }
@@ -40,15 +38,15 @@ pub fn gen(input: &str) -> Vec<Line> {
 #[aoc(day14, part1)]
 pub fn p1(input: &[Line]) -> u64 {
     let mut mem: HashMap<u64, u64> = HashMap::new();
-    let mut mask  = None;
+    let mut mask = None;
     for l in input {
         match l {
             Line::SetMask(m) => {
                 mask = Some(m);
             }
             &Line::SetMem(target, value) => {
-                let v = (value & mask.unwrap().mask) | mask.unwrap().lo;
-                mem.insert(target, v);
+                let m = mask.expect("Mask use before set");
+                mem.insert(target, (value & m.mask) | m.lo);
             }
         }
     }
@@ -56,16 +54,15 @@ pub fn p1(input: &[Line]) -> u64 {
 }
 
 fn get_mem_vals(mask: &Mask, value: u64) -> Vec<u64> {
-    mask.mask.view_bits::<Lsb0>().iter()
-    .zip(mask.lo.view_bits::<Lsb0>())
-    .zip(value.view_bits::<Lsb0>())
-    .enumerate()
-    .fold(vec![0], |acc,(ix,((mask_x,mask_01),value_bit))|{
-        if *mask_x {
-            (0..=1).flat_map(|new_bit| acc.iter().map(move |x| (new_bit << ix) | x)).collect()
+    let mask_bits = mask.mask.view_bits::<Lsb0>();
+    let val_mask = mask.lo | value;
+    mask_bits.iter().enumerate().fold(vec![0], |acc, (ix, &mask_bit)| {
+        if mask_bit {
+            (0..=1)
+                .flat_map(|new_bit| acc.iter().map(move |x| (new_bit << ix) | x))
+                .collect()
         } else {
-            let v = *mask_01 || *value_bit;
-            acc.iter().map(|x| ((v as u64) << ix) | x).collect()
+            acc.iter().map(|x| ((1 << ix) & val_mask) | x).collect()
         }
     })
 }
@@ -81,9 +78,9 @@ pub fn p2(input: &[Line]) -> u64 {
             }
             &Line::SetMem(target, value) => {
                 let ts = get_mem_vals(mask.unwrap(), target);
-                for t in ts {
+                ts.into_iter().for_each(|t| {
                     mem.insert(t, value);
-                }
+                });
             }
         }
     }
