e461506f266ce08cc61efb3525b84b1fbca06e63
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-17 06:08:49 +0000

    day 17

---
+++ 2020/src/sec4/day17.rs
@@ -0,0 +1,56 @@
+use std::collections::{HashMap, HashSet};
+
+use itertools::{Itertools, repeat_n};
+
+use crate::utils::cartesian::as_point_map;
+
+#[allow(clippy::needless_pass_by_value)] //I want a specific type signature here to fit in with the fold where it's used.
+pub fn step(world: HashSet<Vec<i64>>, _step: usize) -> HashSet<Vec<i64>> {
+    let mut counts: HashMap<Vec<i64>, usize> = HashMap::new();
+    for p in &world {
+        for n in (0..p.len()).map(|_| &[-1, 0, 1]).multi_cartesian_product() {
+            let t = p.iter().zip(&n).map(|(a, &b)| a + b).collect();
+            *counts.entry(t).or_default() += 1;
+        }
+    }
+    //Rule in spec: (active && alive neighbours is 2 or 3) || (!active && alive neighbours == 3)
+    //but we've calculated number of alive neighbours + self
+    //so we use rule: (active && count is 3 or 4) || (!active && count is 3)
+    counts
+        .into_iter()
+        .filter_map(|(k, c)| {
+            if c == 3 || c == 4 && world.contains(&k) {
+                Some(k)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+pub fn solve(input: &str, dimensions: usize) -> usize {
+    let input = as_point_map(input, false);
+    let world: HashSet<Vec<i64>> = input
+        .iter()
+        .filter_map(|(p, c)| {
+            if *c == '#' {
+                let mut p = vec![p.x, p.y];
+                p.extend(repeat_n(0, dimensions-2));
+                Some(p)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    (0..6).fold(world, step).len()
+}
+
+#[aoc(day17, part1)]
+pub fn p1(input: &str) -> usize {
+    solve(input, 3)
+}
+
+#[aoc(day17, part2)]
+pub fn p2(input: &str) -> usize {
+    solve(input, 4)
+}
