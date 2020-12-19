d6450cc5192cc2a8b4b97d8f06f9622d1dff209e
tocklime <1396183+tocklime@users.noreply.github.com>
2020-12-19 10:50:31 +0000

    hashmap->vec

--- 2020/src/sec4/day19.rs
+++ 2020/src/sec4/day19.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, num::ParseIntError, str::FromStr};
+use std::{num::ParseIntError, str::FromStr};
 
-use pathfinding::directed::dfs::dfs;
+use pathfinding::prelude::dfs;
 
 #[derive(Debug, Clone)]
 pub enum Match {
@@ -32,18 +32,24 @@ impl FromStr for Rule {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut sp = s.split(": ");
         let id = sp.next().unwrap().parse()?;
-        let matches = sp.next().unwrap().split(" | ").map(str::parse).collect::<Result<Vec<Match>, _>>()?;
+        let matches = sp
+            .next()
+            .unwrap()
+            .split(" | ")
+            .map(str::parse)
+            .collect::<Result<Vec<Match>, _>>()?;
         Ok(Self { id, matches })
     }
 }
 
-pub fn matches(s: &str, rules: &HashMap<usize, Rule>) -> bool {
+pub fn matches(s: &str, rules: &[Rule]) -> bool {
     let chars: Vec<char> = s.chars().collect();
+    let start = (vec![0], 0);
     dfs::<(Vec<usize>, usize), _, _, _>(
-        (vec![0], 0_usize),
+        start,
         |(pending_rules, str_pos)| {
-            if let Some((r,rest)) = pending_rules.split_first() {
-                let rule = &rules[r];
+            if let Some((r, rest)) = pending_rules.split_first() {
+                let rule: &Rule = rules.get(*r).unwrap();
                 rule.matches
                     .iter()
                     .filter_map(|m| -> Option<(Vec<usize>, usize)> {
@@ -70,21 +76,29 @@ pub fn matches(s: &str, rules: &HashMap<usize, Rule>) -> bool {
 #[aoc_generator(day19)]
 pub fn gen(input: &str) -> (Vec<Rule>, Vec<String>) {
     let mut sp = input.split("\n\n");
-    let rules = sp.next().unwrap().lines().map(|l| l.parse::<Rule>().unwrap()).collect();
+    let mut rules: Vec<Rule> = sp.next().unwrap().lines().map(|l| l.parse::<Rule>().unwrap()).collect();
+    rules.sort_by_key(|x| x.id);
+    for (ix, r) in rules.iter().enumerate() {
+        assert_eq!(ix, r.id);
+    }
+
     let candidates = sp.next().unwrap().lines().map(ToString::to_string).collect();
+
     (rules, candidates)
 }
 
 #[aoc(day19, part1)]
-pub fn p1(input: &(Vec<Rule>, Vec<String>)) -> usize {
-    let r_map = input.0.iter().map(|x| (x.id, x.clone())).collect();
-    input.1.iter().filter(|l| matches(l, &r_map)).count()
+pub fn p1((rules, input): &(Vec<Rule>, Vec<String>)) -> usize {
+    input.iter().filter(|l| matches(l, rules)).count()
 }
 
 #[aoc(day19, part2)]
-pub fn p2(input: &(Vec<Rule>, Vec<String>)) -> usize {
-    let mut r_map: HashMap<usize, Rule> = input.0.iter().map(|x| (x.id, x.clone())).collect();
+pub fn p2((rules, input): &(Vec<Rule>, Vec<String>)) -> usize {
+    let mut rules = rules.clone();
     let patch_inp = "8: 42 | 42 8\n11: 42 31 | 42 11 31";
-    r_map.extend(patch_inp.lines().map(|x| x.parse().unwrap()).map(|r: Rule| (r.id, r)));
-    input.1.iter().filter(|l| matches(l, &r_map)).count()
+    for r in patch_inp.lines().map(|x| x.parse::<Rule>().unwrap()) {
+        let id = r.id;
+        rules[id] = r;
+    }
+    input.iter().filter(|l| matches(l, &rules)).count()
 }
