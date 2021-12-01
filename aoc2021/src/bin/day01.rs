30d955b30c4a8e77f7af446db4ed1035e90c6b2c
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-01 06:14:15 +0000

    const generics!

--- aoc2021/src/bin/day01.rs
+++ aoc2021/src/bin/day01.rs
@@ -1,18 +1,16 @@
 use itertools::Itertools;
 
-aoc_harness_macros::aoc_main!(2021 day 1, generator parse_input, [solve(1)] => 1616, [solve(3)] => 1645);
+aoc_harness_macros::aoc_main!(2021 day 1, generator parse_input, [solve::<1>] => 1616, [solve::<3>] => 1645);
 
 fn parse_input(input: &str) -> Vec<u32> {
     input.lines().map(|x| x.parse().unwrap()).collect()
 }
 
-fn solve(n: usize) -> Box<dyn Fn(&[u32]) -> usize> {
-    Box::new(move |input| {
-        input
-            .windows(n)
-            .map(|x| x.iter().sum::<u32>())
-            .tuple_windows()
-            .filter(|(a, b)| b > a)
-            .count()
-    })
+fn solve<const N: usize>(input: &[u32]) -> usize {
+    input
+        .windows(N)
+        .map(|x| x.iter().sum::<u32>())
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count()
 }
