4e514f4b98e1903658255ba27260971a2e48d7bf
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-07 06:22:20 +0000

    using the median/mean trick

--- aoc2021/src/bin/day07.rs
+++ aoc2021/src/bin/day07.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 use aoc_harness::*;
 
 aoc_main!(2021 day 7, generator input::<isize,','>, part1 [p1] => 352997, part2 [p2] => 101571302,
@@ -6,28 +8,27 @@ aoc_main!(2021 day 7, generator input::<isize,','>, part1 [p1] => 352997, part2 
 );
 const EG: &str = "16,1,2,0,4,2,7,1,2,14";
 
-fn fuel_to_pos(input: &[isize], pos: isize) -> isize {
-    input.iter().map(|x| (x - pos).abs()).sum()
+fn p1(input: &[isize]) -> isize {
+    let mut input = input.to_vec();
+    input.sort_unstable();
+    let median = input[input.len() / 2];
+    input
+        .iter()
+        .map(|&x| (x-median).abs())
+        .sum()
 }
 
 fn fuel_to_pos2(input: &[isize], pos: isize) -> isize {
     input
         .iter()
-        .map(|x| {
-            let steps = (x - pos).abs();
+        .map(|&x| {
+            let steps = (x-pos).abs();
             steps * (steps + 1) / 2
         })
         .sum()
 }
-fn p1(input: &[isize]) -> isize {
-    let min = *input.iter().min().unwrap();
-    let max = *input.iter().max().unwrap();
-    (min..=max).map(|p| fuel_to_pos(input, p)).min().unwrap()
-}
 
 fn p2(input: &[isize]) -> isize {
-    let min = *input.iter().min().unwrap();
-    let max = *input.iter().max().unwrap();
-    (min..=max).map(|p| fuel_to_pos2(input, p)).min().unwrap()
+    let mean: isize = input.iter().sum::<isize>() / (input.len() as isize);
+    min(fuel_to_pos2(input, mean), fuel_to_pos2(input, mean + 1))
 }
-
