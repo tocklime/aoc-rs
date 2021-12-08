a38f8a6e27a6deeeda630e5da72310e335abab8a
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-08 14:15:32 +0000

    some tidying

--- aoc2021/src/bin/day08.rs
+++ aoc2021/src/bin/day08.rs
@@ -6,7 +6,7 @@ use utils::numset::NumSet;
 aoc_main!(2021 day 8, generator lines::<Line>, part1 [p1] => 473, part2 [brute_force,intelligence] => 1097568,
   example part1 EG => 26, example part2 EG0 => 5353, example part2 EG => 61229
 );
-type NSet = NumSet<usize>;
+type NSet = NumSet<u8>;
 
 const EG0: &str = "
 acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
@@ -46,12 +46,7 @@ impl FromStr for Line {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut s = s.split(" | ");
-        let wires = s
-            .next()
-            .unwrap()
-            .split(' ')
-            .map(str_to_numset)
-            .collect();
+        let wires = s.next().unwrap().split(' ').map(str_to_numset).collect();
         let lights = s
             .exactly_one()
             .unwrap()
@@ -63,9 +58,10 @@ impl FromStr for Line {
 }
 impl Line {
     pub fn decode(&self) -> usize {
-        let lu : HashMap<NSet,usize> = DIGIT_MAP
+        let lu: HashMap<NSet, usize> = DIGS
             .iter()
-            .map(|x| (str_to_numset(x.1), x.0))
+            .enumerate()
+            .map(|(d, n)| (NumSet::from(*n), d))
             .collect::<HashMap<_, _>>();
         //1 is the only one with 2 lit
         let one = *self.wires.iter().find(|x| x.len() == 2).unwrap();
@@ -76,22 +72,47 @@ impl Line {
         //8 is the only one with 7 lit
         let eight = *self.wires.iter().find(|x| x.len() == 7).unwrap();
         //a is the one lit in 7 but not in 1.
-        let map_a  = (seven - one).iter().exactly_one().unwrap();
+        let map_a = (seven - one).iter().exactly_one().unwrap();
         //6 is the only one with 6 lit s.t. 6 | 1 has 7 lit
-        let six = *self.wires.iter().filter(|x| x.len() == 6 && (**x | one).len() == 7).exactly_one().unwrap();
+        let six = *self
+            .wires
+            .iter()
+            .filter(|x| x.len() == 6 && (**x | one).len() == 7)
+            .exactly_one()
+            .unwrap();
         //c is the one lit in 8 but not in 6.
         let map_c = (eight - six).iter().exactly_one().unwrap();
         //5 is the only one with 5 lit s.t. 6 - 5 has 1 lit (and that lit one is e)
-        let five = *self.wires.iter().filter(|&&x| x.len() == 5 && (six-x).len() == 1).exactly_one().unwrap();
+        let five = *self
+            .wires
+            .iter()
+            .filter(|&&x| x.len() == 5 && (six - x).len() == 1)
+            .exactly_one()
+            .unwrap();
         let map_e = (six - five).iter().exactly_one().unwrap();
         //0 is the only one with length 6 with e lit.
-        let zero = *self.wires.iter().filter(|&&x| x.len() == 6 && x.contains(map_e as usize) && x != six).exactly_one().unwrap();
+        let zero = *self
+            .wires
+            .iter()
+            .filter(|&&x| x.len() == 6 && x.contains(map_e as usize) && x != six)
+            .exactly_one()
+            .unwrap();
         //9 is the other len 6.
         // let nine = *self.wires.iter().filter(|&&x| x.len() == 6 && x != zero && x != six).exactly_one().unwrap();
         //2 has len 5, and e lit.
-        let two = *self.wires.iter().filter(|x| x.len() == 5 && x.contains(map_e as usize)).exactly_one().unwrap();
+        let two = *self
+            .wires
+            .iter()
+            .filter(|x| x.len() == 5 && x.contains(map_e as usize))
+            .exactly_one()
+            .unwrap();
         //3 has len 5, and isn't 2 or 5.
-        let three = *self.wires.iter().filter(|&&x| x.len() == 5 && x != two && x != five).exactly_one().unwrap();
+        let three = *self
+            .wires
+            .iter()
+            .filter(|&&x| x.len() == 5 && x != two && x != five)
+            .exactly_one()
+            .unwrap();
         //d is 8 - 0.
         let map_d = (eight - zero).iter().exactly_one().unwrap();
         let map_b = (five - three).iter().exactly_one().unwrap();
@@ -115,9 +136,10 @@ impl Line {
         out[0] * 1000 + out[1] * 100 + out[2] * 10 + out[3]
     }
     pub fn find(&self) -> usize {
-        let lu : HashMap<NSet,usize> = DIGIT_MAP
+        let lu: HashMap<NSet, usize> = DIGS
             .iter()
-            .map(|x| (str_to_numset(x.1), x.0))
+            .enumerate()
+            .map(|(d, n)| (NumSet::from(*n), d))
             .collect::<HashMap<_, _>>();
         let ans = "abcdefg"
             .chars()
@@ -143,23 +165,12 @@ impl Line {
 }
 fn convert(input: NSet, mapping: &[u8]) -> NSet {
     // dbg!(input, mapping);
-    input
-        .iter()
-        .map(|x| mapping[x as usize])
-        .collect()
+    input.iter().map(|x| mapping[x as usize]).collect()
 }
 
-const DIGIT_MAP: [(usize, &str); 10] = [
-    (0, "abcefg"),
-    (1, "cf"),
-    (2, "acdeg"),
-    (3, "acdfg"),
-    (4, "bcdf"),
-    (5, "abdfg"),
-    (6, "abdefg"),
-    (7, "acf"),
-    (8, "abcdefg"),
-    (9, "abcdfg"),
+const DIGS: [u8; 10] = [
+    0b01110111, 0b00100100, 0b01011101, 0b01101101, 0b00101110, 0b01101011, 0b01111011, 0b00100101,
+    0b01111111, 0b01101111,
 ];
 fn p1(input: &[Line]) -> usize {
     input
