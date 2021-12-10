3f703e75c998bddc47c453ac82180289f23b6f5b
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-10 05:37:25 +0000

    day 10 tidied

--- aoc2021/src/bin/day10.rs
+++ aoc2021/src/bin/day10.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use aoc_harness::*;
 
-aoc_main!(2021 day 10, part1 [p1], part2 [p2],
+aoc_main!(2021 day 10, generator lines::<StackEval>, part1 [p1] => 411471, part2 [p2] => 3122628974,
           example part1 EG => 26397, example part2 EG => 288957);
 
 const EG: &str = "[({(<(())[]>[[{[]{<()<>>
@@ -16,81 +16,45 @@ const EG: &str = "[({(<(())[]>[[{[]{<()<>>
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]";
 
-fn p1(input: &str) -> usize {
-    input
-        .lines()
-        .map(|l| {
-            let mut stack = Vec::new();
-            let mut score = 0;
-            for c in l.chars() {
-                let expected = stack.last();
-                match (c, expected) {
-                    ('(', _) => stack.push(')'),
-                    ('{', _) => stack.push('}'),
-                    ('[', _) => stack.push(']'),
-                    ('<', _) => stack.push('>'),
-                    (a, Some(&b)) if a == b => {
-                        stack.pop().unwrap();
-                    }
-                    (')', _) => {
-                        score += 3;
-                        break;
-                    }
-                    ('}', _) => {
-                        score += 1197;
-                        break;
-                    }
-                    (']', _) => {
-                        score += 57;
-                        break;
-                    }
-                    ('>', _) => {
-                        score += 25137;
-                        break;
-                    }
-                    _ => unreachable!(),
-                }
-            }
-            score
-        })
-        .sum::<usize>()
+enum StackEval {
+    Corrupt(char),
+    Valid(Vec<char>),
 }
+impl FromStr for StackEval {
+    type Err = ();
 
-fn p2(input: &str) -> usize {
-    let mut s = input
-        .lines()
-        .filter_map(|l| {
-            let mut stack = Vec::new();
-            let mut score = 0;
-            for c in l.chars() {
-                let expected = stack.last();
-                match (c, expected) {
-                    ('(', _) => stack.push(')'),
-                    ('{', _) => stack.push('}'),
-                    ('[', _) => stack.push(']'),
-                    ('<', _) => stack.push('>'),
-                    (a, Some(&b)) if a == b => {
-                        stack.pop().unwrap();
-                    }
-                    (')', _) => {
-                        return None;
-                    }
-                    ('}', _) => {
-                        return None;
-                    }
-                    (']', _) => {
-                        return None;
-                    }
-                    ('>', _) => {
-                        return None;
-                    }
-                    _ => unreachable!(),
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            let expected = stack.last();
+            match (c, expected) {
+                ('(', _) => stack.push(')'),
+                ('{', _) => stack.push('}'),
+                ('[', _) => stack.push(']'),
+                ('<', _) => stack.push('>'),
+                (a, Some(&b)) if a == b => {
+                    stack.pop().unwrap();
                 }
+                (x, _) => return Ok(StackEval::Corrupt(x)),
             }
-            Some(stack)
-        })
-        .map(|stack| {
-            let score = stack.iter().rev().fold(0, |s, c| {
+        }
+        Ok(StackEval::Valid(stack))
+    }
+}
+impl StackEval {
+    fn score_corrupt(&self) -> Option<usize> {
+        match self {
+            StackEval::Corrupt(')') => Some(3),
+            StackEval::Corrupt(']') => Some(57),
+            StackEval::Corrupt('}') => Some(1197),
+            StackEval::Corrupt('>') => Some(25137),
+            _ => None,
+        }
+    }
+    fn score_incomplete(&self) -> Option<usize> {
+        match self {
+            StackEval::Corrupt(_) => None,
+            StackEval::Valid(stack) => Some(stack.iter().rev().fold(0, |s, c| {
                 let a = match c {
                     ')' => 1,
                     '}' => 3,
@@ -99,13 +63,19 @@ fn p2(input: &str) -> usize {
                     _ => unreachable!(),
                 };
                 s * 5 + a
-            });
-            score
-        })
-        .collect_vec();
-    s.sort();
-    dbg!(&s);
-    s[s.len() / 2]
+            })),
+        }
+    }
 }
 
-//wrong: 2360429063
+fn p1(input: &[StackEval]) -> usize {
+    input.iter().filter_map(|l| l.score_corrupt()).sum()
+}
+fn p2(input: &[StackEval]) -> usize {
+    let mut s = input
+        .iter()
+        .filter_map(|l| l.score_incomplete())
+        .collect_vec();
+    s.sort();
+    s[s.len() / 2]
+}
