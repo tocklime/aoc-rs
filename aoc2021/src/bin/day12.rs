afeadbbcc9d5a1ac80183d04a482bbbc898fb573
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-12 06:42:27 +0000

    tidied up

--- aoc2021/src/bin/day12.rs
+++ aoc2021/src/bin/day12.rs
@@ -2,7 +2,7 @@ use std::collections::{HashMap, HashSet};
 
 use aoc_harness::*;
 
-aoc_main!(2021 day 12, part1 [p1], part2 [p2], example part1 EG => 10, example part2 EG => 36, example part2 EG2 => 103);
+aoc_main!(2021 day 12, generator gen, part1 [solve::<0>], part2 [solve::<1>], example part1 EG => 10, example part2 EG => 36, example part2 EG2 => 103);
 
 const EG: &str = "start-A
 start-b
@@ -22,49 +22,56 @@ kj-sa
 kj-HN
 kj-dc";
 
-fn explore<'a>(
-    connections: &'a HashMap<String, Vec<String>>,
+struct State<'a> {
+    map: &'a HashMap<String, Vec<String>>,
+    visited: HashSet<&'a str>,
     remaining_small_visits: usize,
-    paths: &'_ mut HashSet<Vec<&'a String>>,
-    path: &'_ mut Vec<&'a String>,
-) {
-    let pos = path.last().unwrap();
-    if let Some(next) = connections.get(*pos) {
-        let valid_targets = next
-            .iter()
-            .filter(|x| {
-                x.chars().next().unwrap().is_ascii_uppercase()
-                    || !path.contains(x)
-                    || remaining_small_visits > 0
-            })
-            .collect_vec();
-        // println!("Gone {:?}, valid targets: {:?}", &path, &valid_targets);
-        for n in valid_targets {
-            let mut new_path = path.clone();
-            new_path.push(n);
-            if n == "end" {
-                // println!("Gone {:?}, found new path {:?}", &path, &new_path);
-                paths.insert(new_path);
-            } else {
-                // println!("Gone {:?}, considering {}", &path, n);
-                let is_small = n.chars().next().unwrap().is_ascii_lowercase();
-                if is_small && path.contains(&n) {
-                    explore(
-                        connections,
-                        remaining_small_visits - 1,
-                        paths,
-                        &mut new_path,
-                    );
-                } else {
-                    explore(connections, remaining_small_visits, paths, &mut new_path);
-                }
-            }
+    pos: &'a str,
+}
+fn is_lower(s: &str) -> bool {
+    s.chars().next().unwrap().is_ascii_lowercase()
+}
+impl<'a> State<'a> {
+    fn new(map: &'a HashMap<String, Vec<String>>, remaining_small_visits: usize) -> Self {
+        Self {
+            map,
+            visited: HashSet::new(),
+            remaining_small_visits,
+            pos: "start",
         }
-    } else {
-        //dead end.
+    }
+    fn options(&'a self) -> impl Iterator<Item = &'a str> {
+        const EMPTY: &Vec<String> = &Vec::new();
+        self.map
+            .get(self.pos)
+            .unwrap_or(EMPTY)
+            .iter()
+            .map(|x| &x[..])
+            .filter(|x| {
+                !is_lower(x) || !self.visited.contains(x) || self.remaining_small_visits > 0
+            })
+    }
+    fn step(&'a self, to: &'a str) -> Self {
+        let mut visited = self.visited.clone();
+        let small_visit_delta: usize = (is_lower(to) && !visited.insert(to)).into();
+        Self {
+            map: self.map,
+            visited,
+            remaining_small_visits: self.remaining_small_visits - small_visit_delta,
+            pos: to,
+        }
+    }
+    fn explore(&'a mut self) -> usize {
+        self.options()
+            .map(|o| match o {
+                "end" => 1,
+                "start" => 0,
+                _ => self.step(o).explore(),
+            })
+            .sum()
     }
 }
-fn p1(input: &str) -> usize {
+fn gen(input: &str) -> HashMap<String, Vec<String>> {
     let mut connections: HashMap<String, Vec<String>> = HashMap::new();
     for l in input.lines() {
         let mut i = l.split('-');
@@ -73,35 +80,9 @@ fn p1(input: &str) -> usize {
         connections.entry(a.clone()).or_default().push(b.clone());
         connections.entry(b).or_default().push(a);
     }
-    let mut paths = HashSet::new();
-    let start = connections
-        .keys()
-        .find(|&x| <String as AsRef<str>>::as_ref(x) == "start")
-        .unwrap();
-    let mut path = vec![start];
-    explore(&connections, 0, &mut paths, &mut path);
-    paths.len()
+    connections
 }
 
-fn p2(input: &str) -> usize {
-    let mut connections: HashMap<String, Vec<String>> = HashMap::new();
-    for l in input.lines() {
-        let mut i = l.split('-');
-        let a = i.next().unwrap().to_string();
-        let b = i.next().unwrap().to_string();
-        if b != "start" {
-            connections.entry(a.clone()).or_default().push(b.clone());
-        }
-        if a != "start" {
-            connections.entry(b).or_default().push(a);
-        }
-    }
-    let mut paths = HashSet::new();
-    let start = connections
-        .keys()
-        .find(|&x| <String as AsRef<str>>::as_ref(x) == "start")
-        .unwrap();
-    let mut path = vec![start];
-    explore(&connections, 1, &mut paths, &mut path);
-    paths.len()
+fn solve<const SMALL_VISITS: usize>(connections: &HashMap<String, Vec<String>>) -> usize {
+    State::new(connections, SMALL_VISITS).explore()
 }
