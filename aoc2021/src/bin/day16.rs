60c1513560766380b58363fe8c0cb597a8fb5873
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-16 06:20:07 +0000

    tidied up.

--- aoc2021/src/bin/day16.rs
+++ aoc2021/src/bin/day16.rs
@@ -1,8 +1,6 @@
-use std::str::FromStr;
-
 use aoc_harness::*;
 
-aoc_main!(2021 day 16, part1 [p1], part2 [p2], example part1 EG => 16);
+aoc_main!(2021 day 16, generator gen, part1 [p1] => 883, part2 [p2] => 1_675_198_555_015, example part1 EG => 16);
 
 const EG: &str = "8A004A801A8002F478";
 
@@ -22,17 +20,17 @@ impl Packet {
         self.version
             + match &self.message {
                 PacketType::LiteralValue(_) => 0,
-                PacketType::Operator(v) => v.iter().map(|x| x.version_sum()).sum(),
+                PacketType::Operator(v) => v.iter().map(Packet::version_sum).sum(),
             }
     }
     fn evaluate(&self) -> usize {
         match &self.message {
             PacketType::LiteralValue(v) => *v,
             PacketType::Operator(inner) => match self.type_id {
-                0 => inner.iter().map(|x| x.evaluate()).sum(),
-                1 => inner.iter().map(|x| x.evaluate()).product(),
-                2 => inner.iter().map(|x| x.evaluate()).min().unwrap(),
-                3 => inner.iter().map(|x| x.evaluate()).max().unwrap(),
+                0 => inner.iter().map(Packet::evaluate).sum(),
+                1 => inner.iter().map(Packet::evaluate).product(),
+                2 => inner.iter().map(Packet::evaluate).min().unwrap(),
+                3 => inner.iter().map(Packet::evaluate).max().unwrap(),
                 5 => (inner[0].evaluate() > inner[1].evaluate()).into(),
                 6 => (inner[0].evaluate() < inner[1].evaluate()).into(),
                 7 => (inner[0].evaluate() == inner[1].evaluate()).into(),
@@ -72,25 +70,24 @@ fn read_multi_bits(iter: &mut impl Iterator<Item = usize>) -> Option<usize> {
 fn read_one_packet(iter: &mut impl Iterator<Item = usize>) -> Option<Packet> {
     let version = read_bits_to_n(iter, 3)?;
     let type_id = read_bits_to_n(iter, 3)?;
-    let message = match type_id {
-        4 => {
-            let n = read_multi_bits(iter)?;
-            PacketType::LiteralValue(n)
-        }
-        _ => {
-            //operator packet
-            let length_type_id = iter.next()?;
-
-            if length_type_id == 0 {
-                let length = read_bits_to_n(iter, 15)?;
-                let content = read_bits_to_vec(iter, length)?;
-                let inner_packets = read_packets(&mut content.into_iter());
-                PacketType::Operator(inner_packets)
-            } else {
-                let packet_count = read_bits_to_n(iter, 11)?;
-                let inner_packets = read_n_packets(iter, packet_count)?;
-                PacketType::Operator(inner_packets)
-            }
+    let message = if type_id == 4 {
+        //literal value packet
+        let n = read_multi_bits(iter)?;
+        PacketType::LiteralValue(n)
+    } else {
+        //operator packet
+        let length_type_id = iter.next()?;
+        if length_type_id == 0 {
+            //bits-based packet contents
+            let length = read_bits_to_n(iter, 15)?;
+            let content = read_bits_to_vec(iter, length)?;
+            let inner_packets = read_packets(&mut content.into_iter());
+            PacketType::Operator(inner_packets)
+        } else {
+            //packet count based packet contents.
+            let packet_count = read_bits_to_n(iter, 11)?;
+            let inner_packets = read_n_packets(iter, packet_count)?;
+            PacketType::Operator(inner_packets)
         }
     };
     Some(Packet {
@@ -114,60 +111,19 @@ fn read_n_packets(iter: &mut impl Iterator<Item = usize>, n: usize) -> Option<Ve
     Some(packets)
 }
 
-fn p1(input: &str) -> usize {
-    let bits_iter = input
-        .trim()
-        .chars()
-        .flat_map(|c| match c {
-            '0' => [0, 0, 0, 0],
-            '1' => [0, 0, 0, 1],
-            '2' => [0, 0, 1, 0],
-            '3' => [0, 0, 1, 1],
-            '4' => [0, 1, 0, 0],
-            '5' => [0, 1, 0, 1],
-            '6' => [0, 1, 1, 0],
-            '7' => [0, 1, 1, 1],
-            '8' => [1, 0, 0, 0],
-            '9' => [1, 0, 0, 1],
-            'A' => [1, 0, 1, 0],
-            'B' => [1, 0, 1, 1],
-            'C' => [1, 1, 0, 0],
-            'D' => [1, 1, 0, 1],
-            'E' => [1, 1, 1, 0],
-            'F' => [1, 1, 1, 1],
-            _ => unreachable!(),
-        })
-        .collect_vec();
-    let mut iter = bits_iter.into_iter();
-    let packets = read_packets(&mut iter);
-    packets.iter().map(|x| x.version_sum()).sum()
+fn gen(input: &str) -> Vec<Packet> {
+    let mut bits_iter = input.trim().chars().flat_map(|c| {
+        let n = usize::from_str_radix(&c.to_string(), 16).unwrap();
+        [n >> 3 & 1, n >> 2 & 1, n >> 1 & 1, n & 1]
+    });
+    let packet = read_packets(&mut bits_iter);
+    assert!(bits_iter.next().is_none());
+    packet
+}
+fn p1(input: &[Packet]) -> usize {
+    input.iter().map(Packet::version_sum).sum()
 }
 
-fn p2(input: &str) -> usize {
-    let bits_iter = input
-        .trim()
-        .chars()
-        .flat_map(|c| match c {
-            '0' => [0, 0, 0, 0],
-            '1' => [0, 0, 0, 1],
-            '2' => [0, 0, 1, 0],
-            '3' => [0, 0, 1, 1],
-            '4' => [0, 1, 0, 0],
-            '5' => [0, 1, 0, 1],
-            '6' => [0, 1, 1, 0],
-            '7' => [0, 1, 1, 1],
-            '8' => [1, 0, 0, 0],
-            '9' => [1, 0, 0, 1],
-            'A' => [1, 0, 1, 0],
-            'B' => [1, 0, 1, 1],
-            'C' => [1, 1, 0, 0],
-            'D' => [1, 1, 0, 1],
-            'E' => [1, 1, 1, 0],
-            'F' => [1, 1, 1, 1],
-            _ => unreachable!(),
-        })
-        .collect_vec();
-    let mut iter = bits_iter.into_iter();
-    let packets = read_packets(&mut iter);
-    packets[0].evaluate()
+fn p2(input: &[Packet]) -> usize {
+    input.iter().map(Packet::evaluate).sum()
 }
