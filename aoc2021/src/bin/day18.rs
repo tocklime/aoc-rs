ac2ceba14d94621b8201392f2255af3c037144ef
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-18 09:22:48 +0000

    some tidying

--- aoc2021/src/bin/day18.rs
+++ aoc2021/src/bin/day18.rs
@@ -2,7 +2,7 @@ use std::{fmt::Display, ops::Add, str::FromStr};
 
 use aoc_harness::*;
 
-aoc_main!(2021 day 18, generator lines::<SnailNumber>, part1 [p1], part2 [p2],  example part1 EG => 4140);
+aoc_main!(2021 day 18, generator lines::<Snail>, part1 [p1] => 4641, part2 [p2] => 4624, example part1 EG => 4140);
 
 const EG: &str = "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
 [[[5,[2,8]],4],[5,[[9,9],0]]]
@@ -16,53 +16,28 @@ const EG: &str = "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
 [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
 
 #[derive(Debug, Clone)]
-struct SnailNumber {
-    body: Body,
-}
-#[derive(Debug, Clone)]
-enum Body {
+enum Snail {
     Leaf(usize),
-    Tree(Children),
+    Pair(Box<(Snail, Snail)>),
 }
-#[derive(Debug, Clone)]
-struct Children {
-    left: Box<SnailNumber>,
-    right: Box<SnailNumber>,
-}
-fn parse_snail(s: &str) -> (SnailNumber, &str) {
+fn parse_snail(s: &str) -> (Snail, &str) {
     match s.chars().next().unwrap() {
         '[' => {
             let (left, s) = parse_snail(&s[1..]);
             assert_eq!(s.as_bytes().get(0), Some(&b','));
             let (right, s) = parse_snail(&s[1..]);
             assert_eq!(s.as_bytes().get(0), Some(&b']'));
-
-            let ans = SnailNumber {
-                body: Body::Tree(Children {
-                    left: Box::new(left),
-                    right: Box::new(right),
-                }),
-            };
-            // match &mut ans.body {
-            //     Body::Leaf(_) => unreachable!(),
-            //     Body::Tree(c) => unsafe {
-            //         std::ptr::write(c.left.parent, ans.as_ptr());
-            //         std::ptr::write(c.right.parent, ans);
-            //     },
-            // }
-
+            let ans = Snail::new_pair(left, right);
             (ans, &s[1..])
         }
         c => (
-            SnailNumber {
-                body: Body::Leaf(usize::try_from(c.to_digit(16).unwrap()).unwrap()),
-            },
+            Snail::Leaf(usize::try_from(c.to_digit(16).unwrap()).unwrap()),
             &s[1..],
         ),
     }
 }
 
-impl FromStr for SnailNumber {
+impl FromStr for Snail {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -71,24 +46,19 @@ impl FromStr for SnailNumber {
         Ok(a)
     }
 }
-impl Add for SnailNumber {
-    type Output = SnailNumber;
+impl Add for Snail {
+    type Output = Snail;
     fn add(self, rhs: Self) -> Self::Output {
-        let mut a = SnailNumber {
-            body: Body::Tree(Children {
-                left: Box::new(self),
-                right: Box::new(rhs),
-            }),
-        };
+        let mut a = Self::new_pair(self, rhs);
         a.reduce();
         a
     }
 }
-impl Display for SnailNumber {
+impl Display for Snail {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self.body {
-            Body::Leaf(n) => f.write_fmt(format_args!("{}", n))?,
-            Body::Tree(c) => f.write_fmt(format_args!("[{},{}]", c.left, c.right))?,
+        match &self {
+            Self::Leaf(n) => f.write_fmt(format_args!("{}", n))?,
+            Self::Pair(c) => f.write_fmt(format_args!("[{},{}]", c.0, c.1))?,
         }
         Ok(())
     }
@@ -98,179 +68,132 @@ enum Dir {
     Left,
     Right,
 }
-impl SnailNumber {
-    fn find_leftmost_at_depth(&self, n: usize) -> Option<SnailNumberView> {
-        match &self.body {
-            Body::Leaf(_) => None,
-            Body::Tree(c) => {
-                let view = SnailNumberView {
-                    current_node: self,
-                    i_am_of_parent: None,
-                    parent: None,
-                };
-                if n == 0 {
-                    return Some(view);
-                }
-                match c.left.find_leftmost_at_depth(n - 1) {
-                    Some(mut p) => {
-                        p.i_am_of_parent = Some(Dir::Left);
-                        p.parent = Some(Box::new(view));
-                        Some(p)
-                    }
-                    None => match c.right.find_leftmost_at_depth(n - 1) {
-                        Some(mut p) => {
-                            p.i_am_of_parent = Some(Dir::Right);
-                            p.parent = Some(Box::new(view));
-                            Some(p)
-                        }
-                        None => None,
-                    },
-                }
-            }
-        }
+impl Snail {
+    fn new_leaf(n: usize) -> Self {
+        Snail::Leaf(n)
+    }
+    fn new_pair(a: Self, b: Self) -> Self {
+        Self::Pair(Box::new((a, b)))
     }
     fn try_split(&mut self) -> bool {
-        match &mut self.body {
-            Body::Leaf(x) if *x >= 10 => {
-                self.body = Body::Tree(Children {
-                    left: Box::new(SnailNumber {
-                        body: Body::Leaf(*x / 2),
-                    }),
-                    right: Box::new(SnailNumber {
-                        body: Body::Leaf(*x / 2 + *x % 2),
-                    }),
-                });
+        match self {
+            Self::Leaf(x) if *x >= 10 => {
+                *self = Self::new_pair(Self::new_leaf(*x / 2), Self::new_leaf(*x / 2 + *x % 2));
                 true
             }
-            Body::Leaf(_) => false,
-            Body::Tree(c) => c.left.try_split() || c.right.try_split(),
+            Self::Leaf(_) => false,
+            Self::Pair(c) => c.0.try_split() || c.1.try_split(),
         }
     }
-    fn try_explode(&mut self, depth: usize) -> Option<(bool, Option<usize>, Option<usize>)> {
-        match &mut self.body {
-            Body::Leaf(_) => None,
-            Body::Tree(c) => {
+    fn try_explode(&mut self, depth: usize) -> Option<(Option<usize>, Option<usize>)> {
+        match self {
+            Self::Leaf(_) => None,
+            Self::Pair(c) => {
                 if depth == 0 {
-                    //explode here.
-                    let l = c.left.get_value().unwrap();
-                    let r = c.right.get_value().unwrap();
-                    return Some((true, Some(l), Some(r)));
+                    //explode here. we assert that the values in c are leaf nodes.
+                    let l = c.0.get_value().unwrap();
+                    let r = c.1.get_value().unwrap();
+                    return Some((Some(l), Some(r)));
                 }
-                let act = c
-                    .left
-                    .try_explode(depth - 1)
-                    .map(|x| (x, Dir::Left))
-                    .or_else(|| c.right.try_explode(depth - 1).map(|x| (x, Dir::Right)));
-                match act {
-                    Some(((reset, l, r), d)) => {
-                        if reset {
-                            match d {
-                                Dir::Left => c.left.body = Body::Leaf(0),
-                                Dir::Right => c.right.body = Body::Leaf(0),
-                            }
-                        }
-                        match &mut self.body {
-                            Body::Leaf(_) => unreachable!(),
-                            Body::Tree(c) => {
-                                if d == Dir::Left && r.is_some() {
-                                    c.right.add_to_edge(Dir::Left, r.unwrap());
-                                    Some((false, l, None))
-                                } else if d == Dir::Right && l.is_some() {
-                                    c.left.add_to_edge(Dir::Right, l.unwrap());
-                                    Some((false, None, r))
-                                } else {
-                                    Some((false, l, r))
-                                }
-                            }
+                //try recursing down each path and remember which way worked.
+                let explosion_result =
+                    c.0.try_explode(depth - 1)
+                        .map(|x| (x, Dir::Left))
+                        .or_else(|| c.1.try_explode(depth - 1).map(|x| (x, Dir::Right)));
+                explosion_result.map(|((l, r), d)| {
+                    if depth == 1 {
+                        //immediate child exploded. replace it.
+                        match d {
+                            Dir::Left => c.0 = Self::new_leaf(0),
+                            Dir::Right => c.1 = Self::new_leaf(0),
                         }
                     }
-                    None => None,
-                }
+                    //try to push the left and right bits to their places.
+                    match (d, l, r) {
+                        (Dir::Left, _, Some(r)) => {
+                            c.1.add_to_edge(Dir::Left, r);
+                            (l, None)
+                        }
+                        (Dir::Right, Some(l), _) => {
+                            c.0.add_to_edge(Dir::Right, l);
+                            (None, r)
+                        }
+                        _ => (l, r),
+                    }
+                })
             }
         }
     }
+    ///Add `val` to the left-most or right-most value in the number.
     fn add_to_edge(&mut self, dir: Dir, val: usize) {
-        match &mut self.body {
-            Body::Leaf(n) => {
+        match self {
+            Self::Leaf(n) => {
                 *n += val;
             }
-            Body::Tree(c) => match dir {
-                Dir::Left => c.left.add_to_edge(dir, val),
-                Dir::Right => c.right.add_to_edge(dir, val),
+            Self::Pair(c) => match dir {
+                Dir::Left => c.0.add_to_edge(dir, val),
+                Dir::Right => c.1.add_to_edge(dir, val),
             },
         }
     }
     fn reduce(&mut self) {
-        loop {
-            // println!("{}", self);
-            if self.try_explode(4).is_none() && !self.try_split() {
-                break;
-            }
-        }
+        while self.try_explode(4).is_some() || self.try_split() {}
     }
-    fn get_children(&self) -> Option<(&Self, &Self)> {
-        match &self.body {
-            Body::Leaf(_) => None,
-            Body::Tree(c) => Some((&c.left, &c.right)),
-        }
-    }
+
     fn get_value(&self) -> Option<usize> {
-        match &self.body {
-            Body::Leaf(l) => Some(*l),
-            Body::Tree(_) => None,
+        match &self {
+            Self::Leaf(l) => Some(*l),
+            Self::Pair(_) => None,
         }
     }
     fn magnitude(&self) -> usize {
-        match &self.body {
-            Body::Leaf(a) => *a,
-            Body::Tree(c) => 3 * c.left.magnitude() + 2 * c.right.magnitude(),
+        match &self {
+            Self::Leaf(a) => *a,
+            Self::Pair(c) => 3 * c.0.magnitude() + 2 * c.1.magnitude(),
         }
     }
 }
 #[derive(Debug)]
 struct SnailNumberView<'a> {
-    current_node: &'a SnailNumber,
+    current_node: &'a Snail,
     i_am_of_parent: Option<Dir>,
     parent: Option<Box<SnailNumberView<'a>>>,
 }
 
-fn p1(input: &[SnailNumber]) -> usize {
-    let mut ans = input[0].clone();
-    for i in &input[1..] {
-        ans = ans + i.clone();
-    }
-    ans.magnitude()
+fn p1(input: &[Snail]) -> usize {
+    input
+        .iter()
+        .cloned()
+        .reduce(|item, accum| item + accum)
+        .unwrap()
+        .magnitude()
 }
-fn p2(input: &[SnailNumber]) -> usize {
+fn p2(input: &[Snail]) -> usize {
     input
         .iter()
         .permutations(2)
-        .map(|v| {
-            let a = v[0].clone() + v[1].clone();
-            a.magnitude()
-        })
+        .map(|v| (v[0].clone() + v[1].clone()).magnitude())
         .max()
         .unwrap()
 }
 #[cfg(test)]
 mod snailtest {
-    use std::str::FromStr;
 
     use super::*;
 
     fn exp(input: &str) -> String {
-        let mut p: SnailNumber = input.parse().unwrap();
+        let mut p: Snail = input.parse().unwrap();
         p.try_explode(4);
         return format!("{}", p);
     }
     fn spl(input: &str) -> String {
-        let mut p: SnailNumber = input.parse().unwrap();
+        let mut p: Snail = input.parse().unwrap();
         p.try_split();
         return format!("{}", p);
     }
     fn add(a: &str, b: &str) -> String {
-        let mut a: SnailNumber = a.parse().unwrap();
-        let b: SnailNumber = b.parse().unwrap();
+        let a: Snail = a.parse().unwrap();
+        let b: Snail = b.parse().unwrap();
         let c = a + b;
         return format!("{}", c);
     }
@@ -295,9 +218,7 @@ mod snailtest {
             "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
         );
     }
-    #[test]
-    fn test_addition() {
-        const SCRIPT: &str = "
+    const ADDITION_SCRIPT: &str = "
   [[[[4,3],4],4],[7,[[8,4],9]]]
 + [1,1]
 = [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
@@ -338,7 +259,9 @@ mod snailtest {
 + [[[[4,2],2],6],[8,7]]
 = [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]";
 
-        for sum in SCRIPT.trim_matches('\n').split("\n\n") {
+    #[test]
+    fn test_addition() {
+        for sum in ADDITION_SCRIPT.trim_matches('\n').split("\n\n") {
             let mut i = sum.lines();
             let a = &i.next().unwrap()[2..];
             let b = &i.next().unwrap()[2..];
