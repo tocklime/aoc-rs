2d12f8fa95c2f0898dc46fa824721045c6bb55ea
tocklime <1396183+tocklime@users.noreply.github.com>
2021-12-21 08:28:40 +0000

    use vec for cache instead of intmap

--- aoc2021/src/bin/day21.rs
+++ aoc2021/src/bin/day21.rs
@@ -1,7 +1,6 @@
 use std::str::FromStr;
 
 use aoc_harness::*;
-use intmap::IntMap;
 
 aoc_main!(2021 day 21, generator whole_input_is::<Game>, part1 [p1] => 598_416, part2 [p2] => 27_674_034_218_179, example part1 EG => 739_785, example part2 EG => 444_356_092_776_315);
 
@@ -31,15 +30,14 @@ impl Game {
         let n2 = self.scores[1] * 10 + (self.positions[1]);
         n1 | n2 << 8
     }
-    fn take_turn(&mut self, roll: u16) -> Option<(usize, usize)> {
-        let p_ix = 0;
+    fn take_turn(&mut self, roll: u16) -> Option<usize> {
         self.turn += 1;
-        self.positions[p_ix] = (self.positions[p_ix] + roll) % 10;
-        let pos = self.positions[p_ix];
+        self.positions[0] = (self.positions[0] + roll) % 10;
+        let pos = self.positions[0];
         let score = if pos == 0 { 10 } else { pos };
-        self.scores[p_ix] += score;
-        if self.scores[p_ix] >= self.target_score {
-            Some((p_ix, 3 * self.turn * usize::from(self.scores[1 - p_ix])))
+        self.scores[0] += score;
+        if self.scores[0] >= self.target_score {
+            Some(3 * self.turn * usize::from(self.scores[1 - 0]))
         } else {
             self.scores.swap(0, 1);
             self.positions.swap(0, 1);
@@ -66,37 +64,37 @@ fn p1(input: &Game) -> usize {
     loop {
         let roll = die.next().unwrap();
         if let Some(x) = g.take_turn(roll) {
-            return x.1;
+            return x;
         }
     }
 }
 
 const ROLLS: [(u16, usize); 7] = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
-fn explore_from(g: &Game, weight: usize, cache: &mut IntMap<[usize; 2]>) -> [usize; 2] {
-    let n: u64 = g.as_int().into();
-    let mut wins = [0, 0];
-    if let Some(w) = cache.get(n) {
-        wins = *w;
-    } else {
+fn explore_from(g: &Game, weight: usize, cache: &mut P2Cache) -> [usize; 2] {
+    let n: usize = g.as_int().into();
+    let mut wins = cache[n];
+    if wins[0] == 0 && wins[1] == 0 {
         for (r, w) in ROLLS {
             let mut g = g.clone();
-            if let Some((p, _)) = g.take_turn(r) {
-                wins[p] += w;
+            if g.take_turn(r).is_some() {
+                wins[0] += w;
             } else {
                 let w = explore_from(&g, w, cache);
                 wins[0] += w[1];
                 wins[1] += w[0];
             }
         }
-        cache.insert(n, wins);
+        cache[n] = wins;
     }
     [wins[0] * weight, wins[1] * weight]
 }
 
+type P2Cache = Box<[[usize; 2]; 1 << 16]>;
+
 fn p2(input: &Game) -> usize {
     let mut g = input.clone();
     g.target_score = 21;
-    let mut cache = IntMap::new();
+    let mut cache = Box::new([[0, 0]; 1 << 16]);
     let ws = explore_from(&g, 1, &mut cache);
     ws.into_iter().max().unwrap()
 }
