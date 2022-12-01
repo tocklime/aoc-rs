0d4e55c1bd6dc3f0dc20c3c81005e835f4a272f4
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-01 05:19:33 +0000

    2022 day 1!

--- aoc/src/solutions/y2022/day01.rs
+++ aoc/src/solutions/y2022/day01.rs
@@ -1,7 +1,32 @@
+use std::cmp::Reverse;
+
 use aoc_harness::*;
 
-aoc_main!(2022 day 1, generator lines::<usize>, part1 [p1]);
+aoc_main!(2022 day 1, generator gen, part1 [solve::<1>] => 67633, part2 [solve::<3>] => 199_628,
+        example both EG => (24000, 45000));
 
-fn p1(input: &[usize]) -> usize {
-    input.len()
+fn gen(input: &str) -> Vec<usize> {
+    input
+        .split("\n\n")
+        .map(|x| x.lines().map(|y| y.parse::<usize>().unwrap()).sum())
+        .collect()
 }
+fn solve<const N: usize>(input: &[usize]) -> usize {
+    input.iter().sorted_by_key(|&&k| Reverse(k)).take(N).sum()
+}
+
+const EG: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+";
