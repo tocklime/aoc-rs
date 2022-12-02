41d14d6ba957f39e16933f2d0ff62c2dffd75973
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-02 05:36:11 +0000

    2022/2 tidied up.

--- aoc/src/solutions/y2022/day02.rs
+++ aoc/src/solutions/y2022/day02.rs
@@ -1,43 +1,33 @@
 use aoc_harness::*;
 
-aoc_main!(2022 day 2, part1 [p1], part2 [p2], example both EG => (15, 12));
+aoc_main!(2022 day 2, part1 [p1] => 11150, part2 [p2] => 8295, example both EG => (15, 12));
 
 fn p1(input: &str) -> usize {
-    let mut score: usize = 0;
-    for l in input.lines() {
-        let (they, me) = l.split_once(' ').unwrap();
-        let they = they.as_bytes()[0] - b'A';
-        let me = me.as_bytes()[0] - b'X';
-        let shape_score = 1 + me;
-        let win_score = match (they, me) {
-            (a, b) if a == b => 3,
-            (0, 1) | (1, 2) | (2, 0) => 6,
-            _ => 0,
-        };
-        score += usize::from(shape_score) + win_score
-    }
-    score
+    input
+        .lines()
+        .map(|l| {
+            let (they, me) = l.split_once(' ').unwrap();
+            let they = usize::from(they.as_bytes()[0] - b'A');
+            let me = usize::from(me.as_bytes()[0] - b'X');
+            let outcome = (4 + me - they) % 3;
+            (1 + me) + (outcome * 3)
+        })
+        .sum()
 }
 
 fn p2(input: &str) -> usize {
-    let mut score: usize = 0;
-    for l in input.lines() {
-        let (they, outcome) = l.split_once(' ').unwrap();
-        let they = they.as_bytes()[0] - b'A';
-        let outcome = outcome.as_bytes()[0] - b'X'; //0: lose, 1: draw, 2: win
-        let me = match (they, outcome) {
-            (a, 0) => (a + 2) % 3,
-            (a, 1) => a,
-            (a, 2) => (a + 1) % 3,
-            _ => unreachable!(),
-        };
-        let shape_score = 1 + me;
-        let win_score = outcome * 3;
-        score += usize::from(shape_score) + usize::from(win_score);
-    }
-    score
+    input
+        .lines()
+        .map(|l| {
+            let (they, me) = l.split_once(' ').unwrap();
+            let they = usize::from(they.as_bytes()[0] - b'A');
+            let outcome = usize::from(me.as_bytes()[0] - b'X'); //0: lose, 1: draw, 2: win
+            let me = (they + outcome + 2) % 3;
+            (1 + me) + (outcome * 3)
+        })
+        .sum()
 }
-// 6331 wrong.
+
 const EG: &str = "A Y
 B X
 C Z
