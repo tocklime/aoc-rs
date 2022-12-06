3d3544acec43a3b1d9681dc42f39cdd24c3774ba
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-06 05:09:42 +0000

    2022/6 as solved.

--- aoc/src/solutions/y2022/day06.rs
+++ aoc/src/solutions/y2022/day06.rs
@@ -1,7 +1,26 @@
+use std::collections::{VecDeque, HashSet};
+
+use itertools::*;
 use aoc_harness::*;
 
-aoc_main!(2022 day 6, part1 [p1]);
+aoc_main!(2022 day 6, part1 [p1], part2 [p2], example both EG => (7,19));
+
+const EG : &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
 
 fn p1(input: &str) -> usize {
-    input.len()
+    input.chars().tuple_windows().enumerate().find(|(ix, (a,b,c,d))| {
+        a != b && a != c && a != d && b != c && b != d && c != d
+    }).unwrap().0 + 4 
 }
+
+fn p2(input: &str) -> usize {
+    for ix in 0..(input.len() - 14) {
+        let set : HashSet<char> = input.chars().skip(ix).take(14).collect();
+        if set.len() == 14 {
+            dbg!(set);
+            return ix + 14
+        }
+    }
+    0
+}
+
