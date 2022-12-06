2bf77713b58c7558d3899e047475c165873dd85d
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-06 06:08:40 +0000

    2022/6 even quicker.

--- aoc/src/solutions/y2022/day06.rs
+++ aoc/src/solutions/y2022/day06.rs
@@ -1,7 +1,7 @@
 use aoc_harness::*;
 use utils::numset::NumSet;
 
-aoc_main!(2022 day 6, part1 [solve::<4>] => 1544, part2 [solve::<14>] => 2145, example both EG => (7,19));
+aoc_main!(2022 day 6, part1 [solve::<4>, solve_by_strides::<4>] => 1544, part2 [solve::<14>, solve_by_strides::<14>] => 2145, example both EG => (7,19));
 
 const EG: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
 
@@ -11,4 +11,19 @@ fn solve<const WINDOW_SIZE: usize>(input: &str) -> usize {
         let mut set = NumSet::<u32>::new();
         bytes[ix-WINDOW_SIZE..ix].iter().all(|c| set.insert(c - b'a'))
     }).unwrap()
+}
+
+fn solve_by_strides<const WINDOW_SIZE: usize>(input: &str) -> usize {
+    let bytes = input.as_bytes();
+    let mut candidate = 0;
+    while candidate < bytes.len() {
+        let mut set = NumSet::<u32>::new();
+        let found_duplicate = bytes[candidate..candidate+WINDOW_SIZE].iter().enumerate().rev().find(|(_, &c)| !set.insert(c - b'a'));
+        if let Some((ix,_)) = found_duplicate {
+            candidate += ix + 1; //move the window to just after the found duplicate.
+        } else {
+            return candidate + WINDOW_SIZE; //easier to reason if candidate is the start of the window, but the solution wants the end of the window.
+        }
+    }
+    0
 }
