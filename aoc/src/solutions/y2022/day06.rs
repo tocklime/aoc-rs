bb5e16bd50fd297a9caff2c07815f9b3e4e359b0
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-06 05:36:45 +0000

    2022/6 more tidying.

--- aoc/src/solutions/y2022/day06.rs
+++ aoc/src/solutions/y2022/day06.rs
@@ -7,16 +7,8 @@ const EG: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
 
 fn solve<const WINDOW_SIZE: usize>(input: &str) -> usize {
     let bytes = input.as_bytes();
-    for ix in 0..(bytes.len() - WINDOW_SIZE) {
+    (WINDOW_SIZE..bytes.len()).find(|&ix| {
         let mut set = NumSet::<u32>::new();
-        for c in &bytes[ix..ix + WINDOW_SIZE] {
-            if !set.insert(*c - b'a') {
-                break;
-            }
-        }
-        if (set.len() as usize) == WINDOW_SIZE {
-            return ix + WINDOW_SIZE;
-        }
-    }
-    0
+        bytes[ix-WINDOW_SIZE..ix].iter().all(|c| set.insert(c - b'a'))
+    }).unwrap()
 }
