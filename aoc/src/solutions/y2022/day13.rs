0eadaa9fcaefd7cbbdbcc60d7ee8e43e7c6256b5
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-13 07:03:30 +0000

    tidied

--- aoc/src/solutions/y2022/day13.rs
+++ aoc/src/solutions/y2022/day13.rs
@@ -1,9 +1,19 @@
+use std::cmp::Ordering;
+
 use aoc_harness::*;
-use nom::{IResult, sequence::{delimited, separated_pair}, bytes::complete::tag, multi::{separated_list1, separated_list0, many1}, combinator::map, branch::alt, character::complete::newline};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{newline, self},
+    combinator::{all_consuming, map},
+    multi::{separated_list0, separated_list1},
+    sequence::{delimited, terminated},
+    IResult,
+};
 
-aoc_main!(2022 day 13, part1 [p1], example both EG => (13,140), part2 [p2]);
+aoc_main!(2022 day 13, generator gen, part1 [p1] => 5625, example both EG => (13,140), part2 [p2] => 23111);
 
-const EG : &str = "[1,1,3,1,1]
+const EG: &str = "[1,1,3,1,1]
 [1,1,5,1,1]
 
 [[1],[2,3,4]]
@@ -28,54 +38,64 @@ const EG : &str = "[1,1,3,1,1]
 [1,[2,[3,[4,[5,6,0]]]],8,9]
 ";
 
-#[derive(Clone,Debug, PartialEq,Eq)]
-enum List {
-    List(Vec<List>),
-    Single(u8)
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum Packet {
+    List(Vec<Packet>),
+    Single(u8),
 }
 
-fn parse_list(input: &str) -> IResult<&str, List> {
-    let (input, l) = alt((
-        map(nom::character::complete::u8, List::Single),
-        map(delimited(tag("["), separated_list0(tag(","), parse_list), tag("]")), List::List)
-    ))(input)?;
-    Ok((input, l))
-}
-fn parse_input(input: &str) -> IResult<&str, Vec<(List,List)>> {
-    separated_list1(tag("\n\n"), separated_pair(parse_list, newline, parse_list))(input)
+fn parse_list(input: &str) -> IResult<&str, Packet> {
+    alt((
+        map(complete::u8, Packet::Single),
+        map(
+            delimited(tag("["), separated_list0(tag(","), parse_list), tag("]")),
+            Packet::List,
+        ),
+    ))(input)
 }
 
-fn cmp_list(a: &List, b: &List) -> std::cmp::Ordering {
-    match (a, b) {
-        (List::Single(l), List::Single(r)) => l.cmp(r),
-        (List::List(l), List::List(r)) => l.cmp(r),
-        (List::List(l), List::Single(r)) => l.cmp(&vec![List::Single(*r)]),
-        (List::Single(l), List::List(r)) => vec![List::Single(*l)].cmp(r)
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Packet::Single(l), Packet::Single(r)) => l.cmp(r),
+            (Packet::List(l), Packet::List(r)) => l.cmp(r),
+            (Packet::List(l), Packet::Single(r)) => l[..].cmp(&[Packet::Single(*r)][..]),
+            (Packet::Single(l), Packet::List(r)) => [Packet::Single(*l)][..].cmp(r),
+        }
     }
 }
 
-impl Ord for List {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        cmp_list(self, other)
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
-impl PartialOrd for List {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(cmp_list(self,other))
-    }
+
+fn gen(input: &str) -> Vec<Vec<Packet>> {
+    all_consuming(terminated(
+        separated_list1(tag("\n\n"), separated_list1(newline, parse_list)),
+        newline,
+    ))(input)
+    .unwrap()
+    .1
 }
-fn p1(input: &str) -> usize {
-    let (_, l) = parse_input(input).unwrap();
-    l.into_iter().zip(1..).filter(|((a,b),_)| a < b).map(|(_, ix)|ix).sum()
+
+fn p1(input: &[Vec<Packet>]) -> usize {
+    input
+        .iter()
+        .zip(1..)
+        .filter(|(l, _)| l[0] < l[1])
+        .map(|(_, ix)| ix)
+        .sum()
 }
-fn p2(input: &str) -> usize {
-    let (_, mut l) = separated_list1(many1(newline), parse_list)(input).unwrap();
-    let (_, a) = parse_list("[[2]]").unwrap();
-    let (_, b) = parse_list("[[6]]").unwrap();
-    l.push(a.clone());
-    l.push(b.clone());
-    l.sort();
-    let (div1,_) = l.iter().find_position(|&l| l == &a).unwrap();
-    let (div2,_) = l.iter().find_position(|&l| l == &b).unwrap();
-    (1+div1)*(1+div2)
+fn p2(input: &[Vec<Packet>]) -> usize {
+    let mut l = input.iter().flatten().collect::<Vec<_>>();
+    let a = parse_list("[[2]]").unwrap().1;
+    let b = parse_list("[[6]]").unwrap().1;
+    l.push(&a);
+    l.push(&b);
+    l.sort_unstable();
+    let div1 = l.iter().position(|&l| l == &a).unwrap();
+    let div2 = l.iter().position(|&l| l == &b).unwrap();
+    (1 + div1) * (1 + div2)
 }
