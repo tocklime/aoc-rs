2b48515e68dbd076c8bd51fb80adfbe62de11283
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-19 20:54:50 +0000

    part way through vec-ifying. <3s.

--- aoc/src/solutions/y2022/day19.rs
+++ aoc/src/solutions/y2022/day19.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{BTreeMap, HashMap},
-    hash::Hash,
-};
+use std::collections::BTreeMap;
 
 use aoc_harness::*;
 use nom::{
@@ -14,70 +11,78 @@ use nom::{
     IResult,
 };
 
-aoc_main!(2022 day 19, part1 [p1] => 1199, part2 [p2] => 3510, example part1 EG => 33);
+aoc_main!(2022 day 19, generator gen, part1 [p1] => 1199, part2 [p2] => 3510, example part1 EG => 33);
 
 const EG: &str = "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
 ";
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Hash, Copy, Clone)]
-enum Resource {
-    Ore,
-    Clay,
-    Obsidian,
-    Geode,
-}
-use num::Integer;
-use Resource::*;
+const ORE: usize = 0;
+const CLAY: usize = 1;
+const OBSIDIAN: usize = 2;
+const GEODE: usize = 3;
+const RESOURCE_COUNT: usize = 4;
+type Resource = usize;
 
+#[derive(Debug, Copy, Clone)]
+struct ResourceCount([u32; RESOURCE_COUNT]);
+
+impl ResourceCount {
+    const fn new() -> Self {
+        ResourceCount([0; RESOURCE_COUNT])
+    }
+}
 #[derive(Debug)]
 struct Blueprint {
     id: u32,
-    costs: HashMap<Resource, HashMap<Resource, u32>>,
-    maximum_demand: BTreeMap<Resource, u32>,
+    costs: [ResourceCount; RESOURCE_COUNT],
+    maximum_demand: ResourceCount,
 }
 fn parse_resource(input: &str) -> IResult<&str, Resource> {
     alt((
-        value(Ore, tag("ore")),
-        value(Clay, tag("clay")),
-        value(Obsidian, tag("obsidian")),
-        value(Geode, tag("geode")),
+        value(ORE, tag("ore")),
+        value(CLAY, tag("clay")),
+        value(OBSIDIAN, tag("obsidian")),
+        value(GEODE, tag("geode")),
     ))(input)
 }
-fn parse_cost(input: &str) -> IResult<&str, (Resource, HashMap<Resource, u32>)> {
+fn parse_cost(input: &str) -> IResult<&str, (Resource, ResourceCount)> {
     //Each XXX robot costs ....
     let (input, output) = delimited(tag("Each "), parse_resource, tag(" robot costs "))(input)?;
-    let (input, costs) = separated_list1(
+    //2 ore and 3 clay
+    let (input, cost_vec) = separated_list1(
         tag(" and "),
         separated_pair(complete::u32, tag(" "), parse_resource),
     )(input)?;
-    Ok((
-        input,
-        (output, costs.into_iter().map(|(a, b)| (b, a)).collect()),
-    ))
+    let mut costs = ResourceCount::new();
+    for (count, res) in cost_vec {
+        costs.0[res] = count;
+    }
+    Ok((input, (output, costs)))
 }
 fn parse_blueprint(input: &str) -> IResult<&str, Blueprint> {
     let (input, id) = delimited(tag("Blueprint "), complete::u32, tag(": "))(input)?;
-    let (input, costs) = separated_list1(tag(". "), parse_cost)(input)?;
+    let (input, costs_vec) = separated_list1(tag(". "), parse_cost)(input)?;
     let (input, _) = tag(".")(input)?;
-    let mut maximum_demand = BTreeMap::new();
-    maximum_demand.insert(Geode, u32::MAX);
-    for (_, cs) in &costs {
-        for (&c, &val) in cs {
-            let x: &mut u32 = maximum_demand.entry(c).or_default();
-            *x = (*x).max(val);
-        }
+    let mut maximum_demand = ResourceCount::new();
+    for res in 0..GEODE {
+        maximum_demand.0[res] = costs_vec.iter().map(|x| x.1 .0[res]).max().unwrap();
+    }
+    maximum_demand.0[GEODE] = u32::MAX;
+    let mut costs = [ResourceCount::new(); RESOURCE_COUNT];
+    for (res, cost) in costs_vec {
+        costs[res] = cost;
     }
     Ok((
         input,
         Blueprint {
             id,
-            costs: costs.into_iter().collect(),
+            costs,
             maximum_demand,
         },
     ))
 }
-#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Hash, Clone)]
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone)]
 struct State {
     robots: BTreeMap<Resource, u32>,
     resources: BTreeMap<Resource, u32>,
@@ -87,18 +92,18 @@ struct State {
 impl State {
     fn start_state(t: u32) -> Self {
         Self {
-            robots: [(Ore, 1)].into_iter().collect(),
+            robots: [(ORE, 1)].into_iter().collect(),
             resources: Default::default(),
             time_left: t,
         }
     }
     fn geode_heuristic(&self) -> u32 {
         //if we built a geode robot for every remaining turn, how many geodes would we get?
-        let geodes_now = self.resources.get(&Geode).copied().unwrap_or_default();
+        let geodes_now = self.resources.get(&GEODE).copied().unwrap_or_default();
         if self.time_left == 0 {
             return geodes_now;
         }
-        let geode_robots = self.robots.get(&Geode).copied().unwrap_or_default();
+        let geode_robots = self.robots.get(&GEODE).copied().unwrap_or_default();
         let existing_robot_production = geode_robots * self.time_left;
         let time_left = self.time_left;
         let new_robot_production = (time_left * (time_left - 1)) / 2;
@@ -119,24 +124,26 @@ impl Blueprint {
     fn try_build(&self, state: &State, robot: Resource) -> Option<State> {
         let mut time_to_enough_resource = 0;
         //no point in building more robots than we can possibly use in one turn!
-        let max_need = self.maximum_demand[&robot];
+        let max_need = self.maximum_demand.0[robot];
         if state.robots.get(&robot).copied().unwrap_or_default() >= max_need {
             return None;
         }
-        for (c, amount) in &self.costs[&robot] {
-            let have_now = state.resources.get(c).copied().unwrap_or_default();
-            if *amount < have_now {
+        for c in 0..RESOURCE_COUNT {
+            // for (c, amount) in &self.costs[robot] {
+            let amount = self.costs[robot].0[c];
+            let have_now = state.resources.get(&c).copied().unwrap_or_default();
+            if amount <= have_now {
                 continue;
             }
             let need = amount - have_now;
-            let rate = *state.robots.get(c)?;
+            let rate = *state.robots.get(&c)?;
             //in X turns I have X * rate more.
             //what is smallest int s.t. X*rate >= need.
             if rate == 0 {
                 //we can't build this yet.
                 return None;
             }
-            let (d, m) = need.div_mod_floor(&rate);
+            let (d, m) = (need / rate, need % rate);
             time_to_enough_resource = time_to_enough_resource.max(d + u32::from(m > 0));
         }
         //need to wait time_to_done steps, then 1 more step while I build the new robot.
@@ -146,7 +153,8 @@ impl Blueprint {
         }
         let mut new_state = self.wait(state, wait_time);
         //produce 1 robot!
-        for (&r, &count) in &self.costs[&robot] {
+        for r in 0..RESOURCE_COUNT {
+            let count = self.costs[robot].0[r];
             // dbg!(state, robot, &new_state, r, count, wait_time, self);
             *new_state.resources.entry(r).or_default() -= count;
         }
@@ -162,7 +170,7 @@ impl Blueprint {
         let ans = if state.geode_heuristic() < *best_known {
             0
         } else {
-            let results = [Geode, Obsidian, Clay, Ore]
+            let results = [GEODE, OBSIDIAN, CLAY, ORE]
                 .iter()
                 .flat_map(|r| self.try_build(&state, *r))
                 //     .collect::<Vec<_>>();
@@ -178,7 +186,7 @@ impl Blueprint {
                     let final_state = self.wait(&state, state.time_left);
                     let geodes = final_state
                         .resources
-                        .get(&Geode)
+                        .get(&GEODE)
                         .copied()
                         .unwrap_or_default();
                     geodes
@@ -192,16 +200,16 @@ impl Blueprint {
     }
 }
 
-fn p1(input: &str) -> u32 {
-    let (_, blueprints) = separated_list1(newline, parse_blueprint)(input).unwrap();
+fn gen(input: &str) -> Vec<Blueprint> {
+    separated_list1(newline, parse_blueprint)(input).unwrap().1
+}
+fn p1(blueprints: &[Blueprint]) -> u32 {
     blueprints.iter().map(|b| b.most_geodes_in(24) * b.id).sum()
 }
 
-fn p2(input: &str) -> u32 {
-    let (_, blueprints) = separated_list1(newline, parse_blueprint)(input).unwrap();
-    blueprints
+fn p2(blueprints: &[Blueprint]) -> u32 {
+    blueprints[0..3]
         .iter()
-        .take(3)
         .map(|b| b.most_geodes_in(32))
         .product()
 }
