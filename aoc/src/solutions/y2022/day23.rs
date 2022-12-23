258ec3b9fe87525508c8e6fd3997efd7497cc134
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-23 06:30:54 +0000

    fxhash is quicker.

--- aoc/src/solutions/y2022/day23.rs
+++ aoc/src/solutions/y2022/day23.rs
@@ -41,16 +41,21 @@ impl<T> Solo<T> {
     }
 }
 
-fn step_world(world: &mut HashSet<Point<i64>>, round_num: usize) -> usize {
-    let mut proposals: HashMap<Point<i64>, Solo<Point<i64>>> = HashMap::new();
+// type MyMap = HashMap<Point<i64>, Solo<Point<i64>>>;
+// type MySet = HashSet<Point<i64>>;
+type MyMap = fxhash::FxHashMap<Point<i64>, Solo<Point<i64>>>;
+type MySet = fxhash::FxHashSet<Point<i64>>;
+
+fn step_world(world: &mut MySet, round_num: usize) -> usize {
+    let mut proposals: MyMap = MyMap::default();
+    //we get neighbours from `neighbours_with_diagonals clockwise from up.
+    // 701
+    // 6#2
+    // 543
     const CHOICES: [[usize; 3]; 4] = [[7, 0, 1], [3, 4, 5], [5, 6, 7], [1, 2, 3]];
     for loc in world.iter() {
-        //clockwise from up.
         let neighbours = loc.neighbours_with_diagonals();
-        let n_map = loc.neighbours_with_diagonals().map(|p| world.get(&p));
-        // 701
-        // 6#2
-        // 543
+        let n_map = neighbours.map(|p| world.get(&p));
         if n_map.iter().any(|x| x.is_some()) {
             let choice = (0..4).find_map(|ix| {
                 let ch = &CHOICES[(ix + round_num) % 4];
@@ -74,20 +79,20 @@ fn step_world(world: &mut HashSet<Point<i64>>, round_num: usize) -> usize {
     moves
 }
 
-fn gen(input: &str) -> HashSet<Point<i64>> {
+fn gen(input: &str) -> MySet {
     as_point_map::<i64>(input, true)
         .into_iter()
         .filter_map(|(p, c)| (c == '#').then_some(p))
         .collect()
 }
-fn p1(input: &HashSet<Point<i64>>) -> usize {
+fn p1(input: &MySet) -> usize {
     let mut world = input.clone();
     let _: usize = (0..10).map(|r| step_world(&mut world, r)).sum();
     let bb: Aabb<i64> = world.iter().collect();
     bb.area() - world.len()
 }
 
-fn p2(input: &HashSet<Point<i64>>) -> usize {
+fn p2(input: &MySet) -> usize {
     let mut world = input.clone();
     (0..).find(|r| step_world(&mut world, *r) == 0).unwrap() + 1
 }
