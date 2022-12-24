3edd40e3fb9a3475e95bff434cd007777de6e205
tocklime <1396183+tocklime@users.noreply.github.com>
2022-12-24 07:03:38 +0000

    350ms...

--- aoc/src/solutions/y2022/day24.rs
+++ aoc/src/solutions/y2022/day24.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use aoc_harness::*;
 use utils::{
@@ -20,34 +20,38 @@ const EG: &str = "#.######
 struct World {
     start: Point<i32>,
     target: Point<i32>,
-    blizzards: HashMap<Dir, HashSet<Point<i32>>>,
+    blizzards: HashMap<Dir, HashMap<i32, Vec<i32>>>,
 }
 
 impl World {
+    fn blizzard_location(blizzard_start: i32, width: i32, direction: i32, time: i32) -> i32 {
+        ((width + blizzard_start + direction * (time % width) - 1) % width) + 1
+    }
     fn space_at(&self, p: Point<i32>, t: i32) -> char {
         let mod_w = self.target.x;
         let mod_h = self.start.y - 1;
         if p == self.target || p == self.start {
             return '.';
         }
-        if p.x == 0 || p.x > self.target.x || p.y == 0 || p.y >= self.start.y {
+        if p.x <= 0 || p.x > self.target.x || p.y <= 0 || p.y >= self.start.y {
             return '#';
         }
-        let blizzards = [
-            self.blizzards[&Dir::Left]
-                .iter()
-                .any(|x| x.y == p.y && p.x == ((mod_w + x.x - (t % mod_w) - 1) % mod_w) + 1),
-            self.blizzards[&Dir::Right]
-                .iter()
-                .any(|x| x.y == p.y && p.x == ((x.x + t - 1) % mod_w) + 1),
-            self.blizzards[&Dir::Up]
-                .iter()
-                .any(|x| x.x == p.x && p.y == ((x.y + t - 1) % mod_h) + 1),
-            self.blizzards[&Dir::Down]
-                .iter()
-                .any(|x| x.x == p.x && p.y == ((mod_h + x.y - (t % mod_h) - 1) % mod_h) + 1),
+        //here, instead of moving every blizzard t steps, we move ourself t steps back towards t=0,
+        let bliz_locs = [
+            (Dir::Left, p.y, Self::blizzard_location(p.x, mod_w, 1, t)),
+            (Dir::Right, p.y, Self::blizzard_location(p.x, mod_w, -1, t)),
+            (Dir::Down, p.x, Self::blizzard_location(p.y, mod_h, 1, t)),
+            (Dir::Up, p.x, Self::blizzard_location(p.y, mod_h, -1, t)),
         ];
-        match blizzards {
+        //then we check if a blizzard of the necessary direction started in that spot.
+        let blizzs = bliz_locs.map(|(d, fixed, p)| {
+            self.blizzards[&d]
+                .get(&fixed)
+                .into_iter()
+                .flatten()
+                .any(|v| *v == p)
+        });
+        match blizzs {
             [false, false, false, false] => '.',
             [true, false, false, false] => '<',
             [false, true, false, false] => '>',
@@ -98,10 +102,14 @@ fn p2(input: &str) -> (i32, i32) {
     let bb: Aabb<i32> = world.keys().collect();
     let start = Point::new(1, bb.top_right.y);
     let target = Point::new(bb.top_right.x - 1, 0);
-    let mut blizzards: HashMap<Dir, HashSet<Point<i32>>> = Default::default();
+    let mut blizzards: HashMap<Dir, HashMap<i32, Vec<i32>>> = Default::default();
     for (point, char) in world.into_iter() {
         if let Some(d) = Dir::try_from_x("^v<>", char) {
-            blizzards.entry(d).or_default().insert(point);
+            let a = blizzards.entry(d).or_default();
+            match d {
+                Dir::Up | Dir::Down => a.entry(point.x).or_default().push(point.y),
+                Dir::Left | Dir::Right => a.entry(point.y).or_default().push(point.x),
+            }
         }
     }
     let world = World {
