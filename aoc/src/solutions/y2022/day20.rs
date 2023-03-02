7f5e74e9250d81b54e6ec089dacbf50917a5acd8
tocklime <1396183+tocklime@users.noreply.github.com>
2023-03-02 13:11:30 +0000

    another datastructure to solve 2022/20 in much the same way.
    using @jwhitham's associative_positive_list crate. Times on my home
    desktop are a chunk better: Part 1 goes from 20ms with a vecdeque
    to 4.5ms with the APL. Part 2 goes from 210ms to 32ms. Thanks @jwhitham!

--- aoc/src/solutions/y2022/day20.rs
+++ aoc/src/solutions/y2022/day20.rs
@@ -1,13 +1,14 @@
 use std::collections::VecDeque;
 
 use aoc_harness::*;
+use associative_positional_list::AssociativePositionalList;
 use nom::{
     character::complete::{self, newline},
     multi::separated_list1,
     IResult,
 };
 
-aoc_main!(2022 day 20, part1 [solve::<1,1>] => 5962, part2 [solve::<10,811589153>] => 9862431387256, example both EG => (3,1623178306));
+aoc_main!(2022 day 20, part1 [solve::<1,1>, solve_with_apl::<1,1>] => 5962, part2 [solve::<10,811589153>, solve_with_apl::<10,811589153>] => 9862431387256, example both EG => (3,1623178306));
 
 const EG: &str = "1
 2
@@ -38,5 +39,31 @@ fn solve<const ITER: usize, const MUL: i64>(input: &str) -> i64 {
     [1000, 2000, 3000]
         .iter()
         .map(|n| d[(zero_ix + n) % d.len()].1)
+        .sum::<i64>()
+}
+
+fn solve_with_apl<const ITER: usize, const MUL: i64>(input: &str) -> i64 {
+    let (_, numbers) = parse_line(input).unwrap();
+    let len = numbers.len();
+    let mut d: AssociativePositionalList<usize> = AssociativePositionalList::new();
+    for ix in 0..len {
+        d.insert(ix, ix);
+    }
+    for _ in 0..ITER {
+        //in order of original index...
+        for ix in 0..len {
+            let curpos = d.find(&ix).unwrap();
+            let value = numbers[ix] * MUL;
+            let len_now = len - 1;
+            let newpos = (((len_now + curpos) as i64) + value).rem_euclid(len_now as i64);
+            d.remove(curpos);
+            d.insert(newpos as usize, ix);
+        }
+    }
+
+    let zero_ix = d.iter().position(|x| numbers[x] == 0).unwrap();
+    [1000, 2000, 3000]
+        .iter()
+        .map(|n| MUL * numbers[d[(zero_ix + n) % d.len()]])
         .sum::<i64>()
 }
