f393d1b1fb5ba8e98c317547cab4a15441914c2a
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-01 06:17:58 +0000

    day 1, tidied up.

--- aoc/src/solutions/y2023/day01.rs
+++ aoc/src/solutions/y2023/day01.rs
@@ -1,56 +1,56 @@
 use aoc_harness::*;
 
-aoc_main!(2023 day 1, part1 [p1], part2 [p2], example part1 EG => 142, example part2 EG2 => 281); 
+aoc_main!(2023 day 1, part1 [p1] => 54877, part2 [p2] => 54100, example part1 EG => 142, example part2 EG2 => 281);
 
-fn p1(_input: &str) -> i32 {
-    let mut total = 0;
-    for l in _input.lines() {
-        let first_digit = l.chars().find(|c| c.is_digit(10)).unwrap();
-        let last_digit = l.chars().rev().find(|c| c.is_digit(10)).unwrap();
-        let str : i32 = (&format!("{first_digit}{last_digit}")).parse().unwrap();
-        total += str;
-    }
-    total
+fn p1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|l| {
+            let first = l.chars().find_map(|c| c.to_digit(10)).unwrap();
+            let last = l.chars().rev().find_map(|c| c.to_digit(10)).unwrap();
+            first * 10 + last
+        })
+        .sum()
 }
 
-const EG : &str = "1abc2
+const EG: &str = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
 
-
-fn find_from_pos(input: &str, pos: usize) -> Option<u8> {
-    println!("Finding from pos {pos} in {input}");
+fn find_from_pos(input: &str, pos: usize) -> Option<u32> {
     let as_bytes = &input.as_bytes()[pos..];
-    let c = as_bytes[0];
-    if c.is_ascii_digit() {
-        return Some(c - b'0');
+    if let Some(d) = char::from(as_bytes[0]).to_digit(10) {
+        return Some(d);
     }
-    match c {
-        b'z' => if as_bytes.starts_with(b"zero") { Some(0) } else { None }
-        b'o' => if as_bytes.starts_with(b"one") {Some(1)} else { None }
-        b't' => if as_bytes.starts_with(b"two") { Some(2) } else if as_bytes.starts_with(b"three") {Some(3)} else {None}
-        b'f' => if as_bytes.starts_with(b"four") { Some(4) } else if as_bytes.starts_with(b"five") {Some(5)} else {None}
-        b's' => if as_bytes.starts_with(b"six") { Some(6) } else if as_bytes.starts_with(b"seven") {Some(7)} else {None}
-        b'e' => if as_bytes.starts_with(b"eight") {Some(8)} else { None }
-        b'n' => if as_bytes.starts_with(b"nine") {Some(9)} else { None }
-        _ => None
+    match &as_bytes[0..3.min(as_bytes.len())] {
+        b"one" => Some(1),
+        b"two" => Some(2),
+        b"thr" => as_bytes.starts_with(b"three").then_some(3),
+        b"fou" => as_bytes.starts_with(b"four").then_some(4),
+        b"fiv" => as_bytes.starts_with(b"five").then_some(5),
+        b"six" => Some(6),
+        b"sev" => as_bytes.starts_with(b"seven").then_some(7),
+        b"eig" => as_bytes.starts_with(b"eight").then_some(8),
+        b"nin" => as_bytes.starts_with(b"nine").then_some(9),
+        _ => None,
     }
-
 }
 
-fn p2(_input: &str) -> i32 {
+fn p2(input: &str) -> u32 {
     let mut total = 0;
-    for l in _input.lines() {
+    for l in input.lines() {
         let first = (0..l.len()).find_map(|i| find_from_pos(l, i)).unwrap();
-        let last = (0..l.len()).rev().find_map(|i| find_from_pos(l, i)).unwrap();
-        let str : i32 = (&format!("{first}{last}")).parse().unwrap();
-        total += str;
+        let last = (0..l.len())
+            .rev()
+            .find_map(|i| find_from_pos(l, i))
+            .unwrap();
+        total += first * 10 + last;
     }
     total
 }
 
-const EG2 : &str = "two1nine
+const EG2: &str = "two1nine
 eightwothree
 abcone2threexyz
 xtwone3four
