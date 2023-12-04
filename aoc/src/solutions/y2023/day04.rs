daff9aa823c4811a88a126bfb37b6ef0d4f0269e
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-04 14:41:39 +0000

    bacon in release mode and day4 using numsets fully.

--- aoc/src/solutions/y2023/day04.rs
+++ aoc/src/solutions/y2023/day04.rs
@@ -1,9 +1,7 @@
 use aoc_harness::aoc_main;
 use nom::{
-    character::complete::{self, multispace1, newline},
-    combinator::eof,
-    multi::separated_list1,
-    sequence::preceded,
+    character::complete::{self, newline, space1},
+    combinator::{eof, success},
 };
 use nom_supreme::{multi::collect_separated_terminated, tag::complete::tag, ParserExt};
 use utils::{
@@ -11,12 +9,12 @@ use utils::{
     numset::NumSet,
 };
 
-aoc_main!(2023 day 4, generator gen, part1 [p1] => 18619, part2 [p2] => 8063216, example both EG => (13, 30));
+aoc_main!(2023 day 4, generator gen, part1 [p1] => 18619, part2 [p2] => 8_063_216, example both EG => (13, 30));
 
 fn gen(input: &str) -> Vec<Card> {
     use nom::Parser;
 
-    collect_separated_terminated(Card::parse, newline, eof.opt_preceded_by(newline))
+    collect_separated_terminated(Card::parse, success(()), eof.opt_preceded_by(newline))
         .parse(input)
         .expect("Parse")
         .1
@@ -25,7 +23,7 @@ fn p1(cards: &[Card]) -> u32 {
     cards.iter().map(Card::score).sum()
 }
 fn p2(cards: &[Card]) -> u32 {
-    let win_map: Vec<usize> = cards.iter().map(|x| x.win_count()).collect();
+    let win_map: Vec<usize> = cards.iter().map(Card::win_count).collect();
     let mut card_counts = vec![1; cards.len()];
     (0..cards.len())
         .map(|index| {
@@ -42,13 +40,12 @@ fn p2(cards: &[Card]) -> u32 {
 #[derive(Debug)]
 struct Card {
     _id: u32,
-    winning: Vec<u8>,
-    have: Vec<u8>,
+    winning: NumSet<u128>,
+    have: NumSet<u128>,
 }
 impl Card {
     fn win_count(&self) -> usize {
-        let num_set: NumSet<u128> = self.winning.iter().cloned().collect();
-        self.have.iter().filter(|&&x| num_set.contains(x)).count()
+        (self.winning & self.have).len().try_into().unwrap()
     }
     fn score(&self) -> u32 {
         self.win_count()
@@ -58,14 +55,18 @@ impl Card {
     }
     fn parse(input: &str) -> IResult<Self> {
         use nom::Parser;
-
         let (input, _id) = complete::u32
-            .preceded_by(tag("Card").cut().terminated(multispace1))
+            .terminated(tag(":"))
+            .terminated(space1)
+            .preceded_by(tag("Card").terminated(space1))
+            .context("Find ID")
             .parse(input)?;
         let (input, winning) =
-            preceded(ws(tag(":")), separated_list1(multispace1, complete::u8))(input)?;
+            collect_separated_terminated(complete::u8, space1, ws(tag("|").terminated(space1)))
+                .parse(input)?;
         let (input, have) =
-            preceded(ws(tag("|")), separated_list1(multispace1, complete::u8))(input)?;
+            nom_supreme::multi::collect_separated_terminated(complete::u8, space1, newline)
+                .parse(input)?;
         Ok((input, Self { _id, winning, have }))
     }
 }
