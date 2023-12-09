9675d1d3b696315cc42d327d2dd3ffe718c20a0c
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-09 12:24:02 +0000

    tidying up.

--- aoc/src/solutions/y2023/day09.rs
+++ aoc/src/solutions/y2023/day09.rs
@@ -2,55 +2,47 @@ use nom::{
     character::complete::{self, newline, space1},
     multi::separated_list1,
 };
-use nom_supreme::{final_parser::final_parser, ParserExt};
-use utils::nom::NomError;
+use nom_supreme::ParserExt;
 use rayon::prelude::*;
+use utils::nom::parse_all;
 
-aoc_harness::aoc_main!(2023 day 9, both [single_thread, multi_thread] => (1_877_825_184, 1108), example both EG => (114,2));
+aoc_harness::aoc_main!(2023 day 9, generator gen, both [single_thread, multi_thread] => (1_877_825_184, 1108), example both EG => (114,2));
 
-fn gen(input: &str) -> Result<Vec<Vec<i64>>, NomError> {
-    let p = separated_list1(
-        newline::<_, NomError>,
-        separated_list1(space1, complete::i64),
+fn gen(input: &str) -> Vec<Vec<i64>> {
+    parse_all(
+        input,
+        separated_list1(newline, separated_list1(space1, complete::i64)).terminated(newline.opt()),
     )
-    .terminated(newline.opt());
-    final_parser(p)(input)
 }
 fn solve_line(mut ns: Vec<i64>) -> (i64, i64) {
     let mut suffix = *ns.last().unwrap();
     let mut prefix = ns[0];
-    for first_sign in [-1, 1].into_iter().cycle() {
-        let mut all_zero = true;
+    let mut mults = [-1, 1].into_iter().cycle();
+    while ns.iter().any(|x| x != &0) {
         for ix in 0..ns.len() - 1 {
             ns[ix] = ns[ix + 1] - ns[ix];
-            if ns[ix] != 0 {
-                all_zero = false;
-            }
         }
         ns.pop();
         suffix += ns[ns.len() - 1];
-        prefix += first_sign * ns[0];
-        if all_zero {
-            break;
-        }
+        prefix += mults.next().unwrap() * ns[0];
     }
     (suffix, prefix)
 }
 
-fn single_thread(input: &str) -> (i64, i64) {
-    gen(input)
-        .expect("parse")
-        .into_iter()
+fn single_thread(input: &[Vec<i64>]) -> (i64, i64) {
+    input
+        .iter()
+        .cloned()
         .map(solve_line)
-        .fold((0, 0), |(a, b), (c, d)| (a + c, b + d))
+        .fold((0, 0), |a, b| (a.0 + b.0, a.1 + b.1))
 }
 
-fn multi_thread(input: &str) -> (i64, i64) {
-    gen(input)
-        .expect("parse")
+fn multi_thread(input: &[Vec<i64>]) -> (i64, i64) {
+    input
+        .to_vec()
         .into_par_iter()
         .map(solve_line)
-        .reduce(|| (0, 0), |(a, b), (c, d)| (a + c, b + d))
+        .reduce(|| (0, 0), |a, b| (a.0 + b.0, a.1 + b.1))
 }
 
 const EG: &str = "0 3 6 9 12 15
