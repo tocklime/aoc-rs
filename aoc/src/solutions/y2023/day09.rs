5c12cf83ea974d1e2dfa72eccf0d2594479e6ace
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-09 05:24:03 +0000

    2023d9 as solved

---
+++ aoc/src/solutions/y2023/day09.rs
@@ -0,0 +1,75 @@
+use itertools::Itertools;
+use nom::{
+    character::complete::{self, space1},
+    multi::separated_list1,
+    Parser,
+};
+use nom_supreme::ParserExt;
+use utils::nom::IResult;
+
+aoc_harness::aoc_main!(2023 day 9, part1 [p1], part2 [p2], example both EG => (114,2));
+
+fn parse_line(input: &str) -> IResult<Vec<i64>> {
+    separated_list1(space1, complete::i64)
+        .all_consuming()
+        .complete()
+        .parse(input)
+}
+fn p1(input: &str) -> i64 {
+    input
+        .lines()
+        .map(|l| {
+            let (_, ns) = parse_line(l).expect("parse");
+            let mut grid = vec![ns];
+            while grid.last().unwrap().iter().any(|&x| x != 0) {
+                let mut i = grid.last().unwrap().iter();
+                let first = *i.next().unwrap();
+                let diffs = i
+                    .scan(first, |state, n| {
+                        let diff = *n - *state;
+                        *state = *n;
+                        Some(diff)
+                    })
+                    .collect_vec();
+                grid.push(diffs);
+            }
+            let pred : i64 = grid.iter().rev().map(|x| x.last().unwrap()).sum();
+            pred
+        })
+        .sum()
+}
+//wrong: 1877828459
+
+const EG: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+";
+
+fn p2(input: &str) -> i64 {
+    input
+        .lines()
+        .map(|l| {
+            let (_, ns) = parse_line(l).expect("parse");
+            let mut grid = vec![ns];
+            while grid.last().unwrap().iter().any(|&x| x != 0) {
+                let mut i = grid.last().unwrap().iter();
+                let first = *i.next().unwrap();
+                let diffs = i
+                    .scan(first, |state, n| {
+                        let diff = *n - *state;
+                        *state = *n;
+                        Some(diff)
+                    })
+                    .collect_vec();
+                grid.push(diffs);
+            }
+            let pred : i64 = grid.iter().rev().fold(0, |acc, x| x.first().unwrap() - acc);
+            println!("{l}");
+            for g in grid {
+                println!("{}",g.iter().map(|x| x.to_string()).join(" "));
+            }
+            println!("{pred}\n\n");
+            pred
+        })
+        .sum()
+}
