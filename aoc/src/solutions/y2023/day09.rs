9436583f054beb23db76b9c32065152ba1df0186
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-09 13:44:53 +0000

    slightly faster without nom.

--- aoc/src/solutions/y2023/day09.rs
+++ aoc/src/solutions/y2023/day09.rs
@@ -1,32 +1,21 @@
-use nom::{
-    character::complete::{self, newline, space1},
-    multi::separated_list1,
-};
-use nom_supreme::ParserExt;
 use rayon::prelude::*;
-use utils::nom::parse_all;
 
 aoc_harness::aoc_main!(2023 day 9, generator gen, both [single_thread, multi_thread] => (1_877_825_184, 1108), example both EG => (114,2));
 
 fn gen(input: &str) -> Vec<Vec<i64>> {
-    parse_all(
-        input,
-        separated_list1(newline, separated_list1(space1, complete::i64)).terminated(newline.opt()),
-    )
+    let parse_line = |l: &str| l.split_whitespace().map(|x| x.parse().unwrap()).collect();
+    input.lines().map(parse_line).collect()
 }
 fn solve_line(mut ns: Vec<i64>) -> (i64, i64) {
-    let mut suffix = *ns.last().unwrap();
-    let mut prefix = ns[0];
-    let mut mults = [-1, 1].into_iter().cycle();
+    let mut ans = (0, 0);
+    let mut mults = [1, -1].into_iter().cycle();
     while ns.iter().any(|x| x != &0) {
-        for ix in 0..ns.len() - 1 {
-            ns[ix] = ns[ix + 1] - ns[ix];
-        }
+        ans.0 += ns[ns.len() - 1];
+        ans.1 += mults.next().unwrap() * ns[0];
+        (0..ns.len() - 1).for_each(|i| ns[i] = ns[i + 1] - ns[i]);
         ns.pop();
-        suffix += ns[ns.len() - 1];
-        prefix += mults.next().unwrap() * ns[0];
     }
-    (suffix, prefix)
+    ans
 }
 
 fn single_thread(input: &[Vec<i64>]) -> (i64, i64) {
