c84fe453358b28a9d6e4b5774aac2e7526103b67
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-09 06:22:55 +0000

    more optimisation.

--- aoc/src/solutions/y2023/day09.rs
+++ aoc/src/solutions/y2023/day09.rs
@@ -1,6 +1,5 @@
-use itertools::Itertools;
 use nom::{
-    character::complete::{self, space1},
+    character::complete::{self, newline, space1},
     multi::separated_list1,
     Parser,
 };
@@ -9,34 +8,41 @@ use utils::nom::IResult;
 
 aoc_harness::aoc_main!(2023 day 9, both [both] => (1_877_825_184, 1108), example both EG => (114,2));
 
-fn parse_line(input: &str) -> IResult<Vec<i64>> {
-    separated_list1(space1, complete::i64)
-        .all_consuming()
-        .complete()
-        .parse(input)
+fn gen(input: &str) -> Vec<Vec<i64>> {
+    let p: IResult<Vec<Vec<i64>>> =
+        separated_list1(newline, separated_list1(space1, complete::i64))
+            .terminated(newline.opt())
+            .all_consuming()
+            .complete()
+            .parse(input);
+    p.expect("parse").1
+}
+fn solve_line(mut ns: Vec<i64>) -> (i64, i64) {
+    let mut suffix = *ns.last().unwrap();
+    let mut first = [0, ns[0]];
+    for first_ix in [0, 1].into_iter().cycle() {
+        let mut all_zero = true;
+        for ix in 0..ns.len() - 1 {
+            ns[ix] = ns[ix + 1] - ns[ix];
+            if ns[ix] != 0 {
+                all_zero = false;
+            }
+        }
+        ns.pop();
+        suffix += ns[ns.len() - 1];
+        first[first_ix] += ns[0];
+        if all_zero {
+            break;
+        }
+    }
+    let prefix = first[1] - first[0];
+    (suffix, prefix)
 }
 fn both(input: &str) -> (i64, i64) {
-    input
-        .lines()
-        .map(|l| {
-            let (_, ns) = parse_line(l).expect("parse");
-            let mut grid = vec![ns];
-            while grid.last().unwrap().iter().any(|&x| x != 0) {
-                let mut i = grid.last().unwrap().iter();
-                let first = *i.next().unwrap();
-                let diffs = i
-                    .scan(first, |state, n| {
-                        let diff = *n - *state;
-                        *state = *n;
-                        Some(diff)
-                    })
-                    .collect_vec();
-                grid.push(diffs);
-            }
-            let (start,end) = grid.iter().rev().fold((0,0), |(start, end), x| (x.first().unwrap() - start, x.last().unwrap() + end));
-            (end,start)
-        })
-        .fold((0,0), |(a,b),(c,d)| (a+c,b+d))
+    let x = gen(input);
+    x.into_iter()
+        .map(solve_line)
+        .fold((0, 0), |(a, b), (c, d)| (a + c, b + d))
 }
 
 const EG: &str = "0 3 6 9 12 15
