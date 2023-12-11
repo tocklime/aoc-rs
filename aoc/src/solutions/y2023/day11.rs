d568217aec82ae5b120a8a4a12097ed84f4f59a9
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-11 06:43:09 +0000

    faster solution doing each dimension separately.

--- aoc/src/solutions/y2023/day11.rs
+++ aoc/src/solutions/y2023/day11.rs
@@ -1,7 +1,48 @@
 use itertools::Itertools;
 use utils::grid2d::{Coord, Grid2d};
 
-aoc_harness::aoc_main!(2023 day 11, part1 [solve::<2>] => 9_623_138, part2 [solve::<1_000_000>] => 726_820_169_514, example part1 EG => 374);
+aoc_harness::aoc_main!(2023 day 11, part1 [solve::<2>, solve_by_dimensions::<2>] => 9_623_138, 
+    part2 [solve::<1_000_000>, solve_by_dimensions::<1_000_000>] => 726_820_169_514, example part1 EG => 374);
+
+#[inline]
+fn quick_mul<F: Fn() -> usize>(a: usize, b: F) -> usize {
+    if a == 0 {
+        0
+    } else {
+        a * b()
+    }
+}
+fn solve_1_dim<const N: usize>(input: &[usize]) -> usize {
+    //we have a list of numbers where each num is the count of galaxies in that cell.
+    //want the sum of cartesian product distances.
+    let dists = (0..input.len())
+        .scan(0, |d, x| {
+            *d += if input[x] == 0 { N } else { 1 };
+            Some(*d)
+        })
+        .collect_vec();
+    (0..input.len())
+        .map(|a| {
+            quick_mul(input[a], || {
+                (0..a)
+                    .map(|b| quick_mul(input[b], || dists[a] - dists[b]))
+                    .sum()
+            })
+        })
+        .sum()
+}
+
+fn solve_by_dimensions<const N: usize>(input: &str) -> usize {
+    let map = Grid2d::from_str(input, |x| x);
+    let s = map.dim();
+    let cols = (0..s.x)
+        .map(|x| (0..s.y).filter(|&y| map[(y, x)] == '#').count())
+        .collect_vec();
+    let rows = (0..s.y)
+        .map(|y| (0..s.x).filter(|&x| map[(y, x)] == '#').count())
+        .collect_vec();
+    solve_1_dim::<N>(&cols) + solve_1_dim::<N>(&rows)
+}
 
 fn solve<const N: usize>(input: &str) -> usize {
     let map = Grid2d::from_str(input, |x| x);
