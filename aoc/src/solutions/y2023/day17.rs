1431ce2bb3f5b07a7f8f4f0d1c4a47d6212ecd1e
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-17 05:59:55 +0000

    2023 day 17 tidied up some.

--- aoc/src/solutions/y2023/day17.rs
+++ aoc/src/solutions/y2023/day17.rs
@@ -4,84 +4,73 @@ use utils::{
     grid2d::Grid2d,
 };
 
-aoc_harness::aoc_main!(2023 day 17, part1 [p1], part2 [p2], example both EG => (102, 94), example part2 EG2 => 71);
+aoc_harness::aoc_main!(2023 day 17,
+    part1 [solve_astar::<0,3>,solve_dijk::<0, 3>] => 1110,
+    part2 [solve_astar::<4,10>,solve_dijk::<4,10>] => 1294,
+    example both EG => (102, 94), example part2 EG2 => 71);
 
-fn p1(input: &str) -> u32 {
+fn solve_astar<const MIN_STRAIGHT_LINE: usize, const MAX_STRAIGHT_LINE: usize>(input: &str) -> u32 {
     let g = Grid2d::<u32>::from_str(input, |x| ((x as u8) - b'0') as u32);
-
-    let x = dijkstra(
-        &(Point::new(0, 0), Dir::Right, 0u32),
+    let target = Point::new(g.dim().x-1,g.dim().y-1);
+    astar(
+        &(Point::new(0, 0), Dir::Right, 0),
         |&(loc, dir, dir_count)| {
             //right, left or straight on.
             let mut ans = Vec::new();
-            if let Some(&x) = g.get(loc.step_flip_y(dir.turn_left())) {
-                ans.push(((loc.step_flip_y(dir.turn_left()), dir.turn_left(), 0), x));
-            }
-            if dir_count < 2 {
-                if let Some(&x) = g.get(loc.step_flip_y(dir)) {
-                    ans.push(((loc.step_flip_y(dir), dir, dir_count + 1), x));
+            if dir_count == 0 || dir_count >= MIN_STRAIGHT_LINE {
+                //turning OK.
+                for d in [dir.turn_left(), dir.turn_right()] {
+                    let new_pos = loc.step(d);
+                    if let Some(&x) = g.get(new_pos) {
+                        ans.push(((new_pos, d, 1), x));
+                    }
                 }
             }
-            if let Some(&x) = g.get(loc.step_flip_y(dir.turn_right())) {
-                ans.push(((loc.step_flip_y(dir.turn_right()), dir.turn_right(), 0), x));
-            }
-            ans
-        },
-        |x| x.0 == Point::new(g.dim().x - 1, g.dim().y - 1),
-    )
-    .unwrap();
-    let mut a = Grid2d::from_fn(g.dim(), |p| g[p].to_string().chars().next().unwrap());
-    for (p, d, _) in &x.0 {
-        a[*p] = d.map(['^', 'v', '<', '>']);
-    }
-    println!("{a}");
-
-    // dbg!(&x);
-    let ans = x.1;
-
-    ans
-}
-fn p2(input: &str) -> u32 {
-    let g = Grid2d::<u32>::from_str(input, |x| ((x as u8) - b'0') as u32);
-
-    let x = dijkstra(
-        &(Point::new(0, 0), Dir::Right, 0u32),
-        |&(loc, dir, dir_count)| {
-            //right, left or straight on.
-            let mut ans = Vec::new();
-            if dir_count == 0 || dir_count >= 4 {
-                if let Some(&x) = g.get(loc.step_flip_y(dir.turn_left())) {
-                    ans.push(((loc.step_flip_y(dir.turn_left()), dir.turn_left(), 1), x));
-                }
-            }
-            if dir_count < 10 {
-                if let Some(&x) = g.get(loc.step_flip_y(dir)) {
-                    ans.push(((loc.step_flip_y(dir), dir, dir_count + 1), x));
-                }
-            }
-            if dir_count == 0 || dir_count >= 4 {
-                if let Some(&x) = g.get(loc.step_flip_y(dir.turn_right())) {
-                    ans.push(((loc.step_flip_y(dir.turn_right()), dir.turn_right(), 1), x));
+            if dir_count < MAX_STRAIGHT_LINE {
+                //straight line OK.
+                let new_pos = loc.step(dir);
+                if let Some(&x) = g.get(new_pos) {
+                    ans.push(((new_pos, dir, dir_count + 1), x));
                 }
             }
             ans
         },
-        |x| x.2 >= 4 && x.0 == Point::new(g.dim().x - 1, g.dim().y - 1),
+        |(loc, _, _)| loc.manhattan_unsigned(&target) as u32,
+        |x| x.2 >= MIN_STRAIGHT_LINE && x.0 == target
     )
-    .unwrap();
-    let mut a = Grid2d::from_fn(g.dim(), |p| g[p].to_string().chars().next().unwrap());
-    for (p, d, _) in &x.0 {
-        a[*p] = ' ';//d.map(['^', 'v', '<', '>']);
-    }
-    println!("{a}");
-
-    // dbg!(&x);
-    let ans = x.1;
-
-    ans
+    .unwrap()
+    .1
 }
-//1289, 1304: wrong.
-
+fn solve_dijk<const MIN_STRAIGHT_LINE: usize, const MAX_STRAIGHT_LINE: usize>(input: &str) -> u32 {
+    let g = Grid2d::<u32>::from_str(input, |x| ((x as u8) - b'0') as u32);
+    dijkstra(
+        &(Point::new(0, 0), Dir::Right, 0),
+        |&(loc, dir, dir_count)| {
+            //right, left or straight on.
+            let mut ans = Vec::new();
+            if dir_count == 0 || dir_count >= MIN_STRAIGHT_LINE {
+                //turning OK.
+                for d in [dir.turn_left(), dir.turn_right()] {
+                    let new_pos = loc.step(d);
+                    if let Some(&x) = g.get(new_pos) {
+                        ans.push(((new_pos, d, 1), x));
+                    }
+                }
+            }
+            if dir_count < MAX_STRAIGHT_LINE {
+                //straight line OK.
+                let new_pos = loc.step(dir);
+                if let Some(&x) = g.get(new_pos) {
+                    ans.push(((new_pos, dir, dir_count + 1), x));
+                }
+            }
+            ans
+        },
+        |x| x.2 >= MIN_STRAIGHT_LINE && x.0 == Point::new(g.dim().x - 1, g.dim().y - 1),
+    )
+    .unwrap()
+    .1
+}
 
 const EG: &str = "2413432311323
 3215453535623
