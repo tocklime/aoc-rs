608fe9e54aa095b431d3fbcc165967d69e8bd485
tocklime <1396183+tocklime@users.noreply.github.com>
2023-12-22 18:08:54 +0000

    2023d22 under 1s, at least...

--- aoc/src/solutions/y2023/day22.rs
+++ aoc/src/solutions/y2023/day22.rs
@@ -26,6 +26,7 @@ struct World {
     space: Array3<Option<usize>>,
     bricks: Vec<Brick>,
     falls: Vec<usize>,
+    removed: Option<usize>,
 }
 
 impl Brick {
@@ -38,35 +39,66 @@ impl Brick {
             .multi_cartesian_product()
             .map(move |x| Dim([x[0], x[1], x[2] - fall_dist]))
     }
-
-    fn below(&self, w: &World) -> HashSet<usize> {
-        self.blocks(0)
-            .flat_map(|x| (x[2] > 0).then(|| w.space[x - Dim([0, 0, 1])]))
-            .flatten()
-            .collect()
-    }
 }
 
 impl World {
-fn remove(&mut self, brick: usize) {
-    let fall_dist = self.falls[brick];
-    for b in self.bricks[brick].blocks(fall_dist) {
-        assert_eq!(self.space[b], Some(brick));
-        self.space[b] = None;
+    fn below(&self, b: usize) -> HashSet<usize> {
+        let br = &self.bricks[b];
+        let fall = self.falls[b];
+        br.blocks(fall)
+            .flat_map(|x| (x[2] > 0).then(|| self.space[x - Dim([0, 0, 1])]))
+            .flatten()
+            .filter(|x| *x != b)
+            .collect()
     }
-}
-fn do_drops<'a>(&mut self) {
-    for z in 0..self.space.dim().2 {
-        let mut anything_fell = false;
-        todo!();
-        
-        let max_fall = (0..z).take_while(|x| could_place(&self, brick, fall_dist))
-
-        if !anything_fell {
-            break;
+    fn remove(&mut self, brick: usize) {
+        let fall_dist = self.falls[brick];
+        for b in self.bricks[brick].blocks(fall_dist) {
+            assert_eq!(self.space[b], Some(brick));
+            self.space[b] = None;
         }
     }
-}
+    fn do_drops<'a>(&mut self) -> usize {
+        // println!("Doing drops");
+        let mut total_moves = 0;
+        loop {
+            let mut any_this_loop = false;
+            for b in &self.bricks {
+                if Some(b.id) == self.removed {
+                    continue;
+                }
+                let current_fall = self.falls[b.id];
+                let z = b.min_z();
+                let max_fall = (current_fall + 1..z)
+                    .take_while(|x| could_place(&self, b.id, *x))
+                    .last();
+                if let Some(dist) = max_fall {
+                    let old_fall_dist = self.falls[b.id];
+                    any_this_loop = true;
+                    total_moves += 1;
+                    // println!("Dropping {} from {old_fall_dist} to {dist}", b.id);
+                    // draw_world(self);
+                    for bl in b.blocks(old_fall_dist) {
+                        assert_eq!(
+                            self.space[bl],
+                            Some(b.id),
+                            "brick {b:?} is not where it should be"
+                        );
+                        self.space[bl] = None;
+                    }
+                    self.falls[b.id] = dist;
+                    for bl in self.bricks[b.id].blocks(dist) {
+                        assert_eq!(self.space[bl], None);
+                        self.space[bl] = Some(b.id);
+                    }
+                }
+            }
+            if !any_this_loop {
+                break;
+            }
+        }
+        total_moves
+    }
 }
 fn parse_brick(input: &str) -> IResult<Brick> {
     let (input, (a, b)): (&str, ([usize; 3], [usize; 3])) = separated_pair(
@@ -92,23 +124,24 @@ fn could_place(world: &World, brick: usize, fall_dist: usize) -> bool {
 }
 
 fn place(world: &mut World, brick: usize, fall_dist: usize) {
+    world.falls[brick] = fall_dist;
     for b in world.bricks[brick].blocks(fall_dist) {
         assert!(world.space[b].is_none());
         world.space[b] = Some(brick);
     }
 }
 
-
 #[allow(dead_code)]
 fn draw_world(world: &World) {
     for z in 0..10 {
         println!("z: {z}");
-        for y in 0..3 {
-            for x in 0..3 {
+        for y in 0..10 {
+            for x in 0..10 {
                 print!(
                     "{}",
                     if let Some(b) = world.space[[x, y, z]] {
-                        (b'A' + u8::try_from(b % 10).unwrap()) as char
+                        (b % 10).to_string().chars().next().unwrap()
+                        // (b'A' + u8::try_from(b % 10).unwrap()) as char
                     } else {
                         '.'
                     }
@@ -121,16 +154,15 @@ fn draw_world(world: &World) {
 }
 
 impl World {
-    fn new(bricks: Vec<Brick>, without: Option<usize>) -> Self {
+    fn new(bricks: Vec<Brick>) -> Self {
         let mut world = World {
             space: Array3::default([10, 10, 400]),
             falls: vec![0; bricks.len()],
             bricks,
+            removed: None,
         };
-        for br in (0..world.bricks.len()) {
-            if Some(br) != without {
-                place(&mut world, br, 0);
-            }
+        for br in 0..world.bricks.len() {
+            place(&mut world, br, 0);
         }
         world
     }
@@ -142,56 +174,41 @@ fn both(input: &str) -> (usize, usize) {
         .complete()
         .parse(input)
         .expect("parse");
+    bricks.sort_by_key(|x| x.min_z());
     for (ix, b) in bricks.iter_mut().enumerate() {
         b.id = ix;
     }
-    bricks.sort_by_key(Brick::min_z);
-    let world = World::new(bricks, None);
+    // bricks.sort_by_key(Brick::min_z);
+    let mut world = World::new(bricks);
+    world.do_drops();
     //can we remove this?
     //removable iff it's not the only brick supporting something.
     //build a map of (A is supported by [B,C,D])
     //then a brick is not removable if its ever alone on the rhs in that map.
-    let mut supported_by: HashMap<&Brick, HashSet<usize>> = HashMap::new();
-    for b in &world.bricks {
-        let fall = world.falls[b.id];
-        for block in b.blocks(fall) {
-            if block[2] > 1 {
-                if let Some(supp) = world.space[block - Dim([0, 0, 1])] {
-                    if supp != b.id {
-                        supported_by.entry(b).or_default().insert(supp);
-                    }
-                }
-            }
-        }
-    }
+    let supported_by: HashMap<&Brick, HashSet<usize>> = world
+        .bricks
+        .iter()
+        .map(|b| (b, world.below(b.id)))
+        .collect();
     //a is unsafe because it is the only brick supporting...
-    let mut unsafe_to_remove: HashMap<usize, Vec<&Brick>> = HashMap::new();
-    for (supported, supporters) in &supported_by {
+    let mut unsafe_to_remove: HashSet<usize> = HashSet::new();
+    for supporters in supported_by.values() {
         if supporters.len() == 1 {
             let supporter = *supporters.iter().next().unwrap();
-            unsafe_to_remove
-                .entry(supporter)
-                .or_default()
-                .push(*supported);
+            unsafe_to_remove.insert(supporter);
         }
     }
     let p1 = world.bricks.len() - unsafe_to_remove.len();
+    // draw_world(&world);
 
-    progress_bar::init_progress_bar(unsafe_to_remove.len());
     let p2 = unsafe_to_remove
         .par_iter()
-        .map(|(x, _)| {
-            progress_bar::inc_progress_bar();
+        // .iter()
+        .map(|x| {
             let mut new_world = world.clone();
             new_world.remove(*x);
-            new_world.do_drops();
-            new_world
-                .falls
-                .iter()
-                .zip(&world.falls)
-                .filter(|(a, b)| a != b)
-                .count()
-            // new_world.falls.iter().enumerate().filter(|(k, v)| world.falls[**k] != **v).count()
+            new_world.removed = Some(*x);
+            new_world.do_drops()
         })
         .sum();
 
