7d7ca9ac594601bdef3720f6f8f0324db62fc902
Greg Manning <greg@gregmanning.uk>
2024-12-17 23:48:23 +0000

    goes much quicker if you do it backward...

--- aoc/src/solutions/y2024/day17.rs
+++ aoc/src/solutions/y2024/day17.rs
@@ -1,12 +1,12 @@
 use itertools::Itertools;
 use utils::inputs;
 
-aoc_harness::aoc_main!(2024 day 17, 
-    generator Machine::from_str, 
-    part1 [p1] => "2,7,4,7,2,1,7,5,1", 
-    part2 [p2_bfs, p2_dfs,p2_dfs_2] => 37_221_274_271_220,
+aoc_harness::aoc_main!(2024 day 17,
+    generator Machine::from_str,
+    part1 [p1] => "2,7,4,7,2,1,7,5,1",
+    part2 [p2_bfs, p2_dfs,p2_dfs_2, p2_backwards] => 37_221_274_271_220,
     example part1 EG => "4,6,3,5,6,3,5,2,1,0",
-    example part2 EG2 => 117_440
+    // example part2 EG2 => 117_440
 );
 
 #[derive(Debug, Clone)]
@@ -15,7 +15,6 @@ struct Machine {
     ip: usize,
     program: Vec<u8>,
     output: Vec<u8>,
-    force_quine: bool,
 }
 
 impl Machine {
@@ -31,7 +30,6 @@ impl Machine {
             ip: 0,
             program,
             output: Vec::new(),
-            force_quine: false,
         }
     }
     fn read_lit(&self) -> Option<i64> {
@@ -71,13 +69,6 @@ impl Machine {
             }
             5 => {
                 let n = self.read_combo()?.rem_euclid(8) as u8;
-                if self.force_quine {
-                    match self.program.get(self.output.len()) {
-                        None => return None, //output too long.
-                        Some(&x) if x != n => return None, //output value wrong.
-                        _ => (),
-                    }
-                }
                 self.output.push(n);
             }
             6 => {
@@ -101,25 +92,37 @@ impl Machine {
     fn run(&mut self) {
         while self.step().is_some() {}
     }
-    fn quine_score(&self, init_a: i64) -> usize {
-        let mut m = self.clone();
-        // m.force_quine = true;
-        m.regs[0] = init_a;
-        m.run();
-        if m.output.len() > m.program.len() {
+    fn run_with_a(&mut self, init_a: i64) {
+        self.regs[0] = init_a;
+        self.run();
+    }
+    fn match_start(&self) -> usize {
+        if self.output.len() > self.program.len() {
             return 0;
         }
-        let matching = m
+        self
             .output
             .iter()
-            .zip(m.program.iter())
+            .zip(self.program.iter())
             .take_while(|(a, b)| a == b)
-            .count();
-        // println!(
-        //     "   Running with {init_a:o} gets {:?}, which matches first {matching} of program {:?}",
-        //     &m.output, &m.program
-        // );
-        matching
+            .count()
+    }
+    fn match_end(&self) -> usize {
+        if self.output.len() > self.program.len() {
+            return 0;
+        }
+        self
+            .output
+            .iter().rev()
+            .zip(self.program.iter().rev())
+            .take_while(|(a, b)| a == b)
+            .count()
+
+    }
+    fn quine_score(&self, init_a: i64) -> usize {
+        let mut m = self.clone();
+        m.run_with_a(init_a);
+        m.match_start()
     }
 }
 
@@ -135,19 +138,22 @@ fn p2_bfs(m: &Machine) -> i64 {
     let lookahead = if m.program.len() == 6 { 0 } else { 4 };
     let mut fringe: Vec<i64> = [0].into_iter().collect();
     for step_ix in 0..m.program.len() {
-        fringe = fringe.into_iter().flat_map(move |s| {
-            (0..8).filter_map(move |x| {
-                //considering adding `x` as new octal digit on the front of the value.
-                //it's ok if by also adding any possible value between 0 and 2^lookahead 
-                //gives a new correct digit.
-                let with_la = (0..2_i64.pow(lookahead)).any(|la| {
-                    let f = la << 3 | x;
-                    let candidate = s | f << (3*step_ix);
-                    m.quine_score(candidate) >= step_ix
-                });
-                (with_la).then_some(s | x << (3*step_ix))
+        fringe = fringe
+            .into_iter()
+            .flat_map(move |s| {
+                (0..8).filter_map(move |x| {
+                    //considering adding `x` as new octal digit on the front of the value.
+                    //it's ok if by also adding any possible value between 0 and 2^lookahead
+                    //gives a new correct digit.
+                    let with_la = (0..2_i64.pow(lookahead)).any(|la| {
+                        let f = la << 3 | x;
+                        let candidate = s | f << (3 * step_ix);
+                        m.quine_score(candidate) >= step_ix
+                    });
+                    (with_la).then_some(s | x << (3 * step_ix))
+                })
             })
-        }).collect();
+            .collect();
         // println!("Fringe at step {step_ix} now has {}", fringe.len());
     }
     fringe.into_iter().min().unwrap()
@@ -183,7 +189,7 @@ fn p2_dfs_2(m: &Machine) -> i64 {
             return s;
         }
         if next_digit_to_try < 7 {
-            stack.push((s,next_digit_to_try+1,step_ix));
+            stack.push((s, next_digit_to_try + 1, step_ix));
         }
         //considering adding `next_digit_to_try` as new octal digit on the front of the value.
         //it's ok if by also adding any possible value between 0 and 2^lookahead
@@ -194,7 +200,28 @@ fn p2_dfs_2(m: &Machine) -> i64 {
             m.quine_score(candidate) >= step_ix
         });
         if with_la {
-            stack.push((s|next_digit_to_try << (3*step_ix),0,step_ix+1));
+            stack.push((s | next_digit_to_try << (3 * step_ix), 0, step_ix + 1));
+        }
+    }
+    0
+}
+fn p2_backwards(m: &Machine) -> i64 {
+    let mut stack: Vec<(i64, i64, usize)> = [(0, 0, 0)].into_iter().collect();
+    while let Some((s, next_digit_to_try, step_ix)) = stack.pop() {
+        if next_digit_to_try < 7 {
+            stack.push((s, next_digit_to_try + 1, step_ix));
+        }
+        //considering adding `next_digit_to_try` as new octal digit on the end (lsb) of the value.
+        //it's good if it outputs at least the last `step_ix` digits.
+        let candidate = s << 3 | next_digit_to_try;
+        let mut m = m.clone();
+        m.run_with_a(candidate);
+        let score = m.match_end();
+        if score == m.program.len() {
+            return candidate;
+        }
+        if score >= step_ix {
+            stack.push((candidate, 0, step_ix + 1));
         }
     }
     0
