062fd1c8ac2f90cbccc3c8532982746b71f74dd1
Greg Manning <greg@gregmanning.uk>
2024-12-24 15:40:58 +0000

    2024d21 finally solved. Taking a break now until after Christmas

--- aoc/src/solutions/y2024/day21.rs
+++ aoc/src/solutions/y2024/day21.rs
@@ -1,85 +1,131 @@
-use itertools::Itertools;
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
 use utils::{
-    cartesian::{Dir, Point},
-    grid2d::Grid2d,
+    cartesian::{Dir, Point}, collections::VecLookup, grid2d::Grid2d
 };
 
-aoc_harness::aoc_main!(2024 day 21, part1 [p1],
-    example part1 "456A" => 64 * 456,
-    example part1 "379A" => 64 * 379,
+aoc_harness::aoc_main!(2024 day 21, 
+    part1 [p1::<3>] => 202_648, 
+    // part2 [p1::<16>, p1_shorter::<16>],
+    // part2 [p1_shorter::<26>],
+    part2 [p1::<26>] => 248_919_739_734_728,
     example part1 "029A" => 68 * 29,
+    example part1 "379A" => 64 * 379,
+    example part1 "456A" => 64 * 456,
     example part1 "980A" => 60 * 980,
     example part1 "179A" => 68 * 179,
     example part1 EG => 126_384,
 );
 
-#[derive(Clone)]
 struct ButtonPad {
     grid: Grid2d<char>,
-    position: Point<usize>,
 }
-const DIR_PRIORITY : [Dir;4] = [Dir::Down, Dir::Left, Dir::Right, Dir::Up];
+
+type Cache = VecLookup<HashMap<(char,char),usize>>;
+
+#[allow(dead_code)]
+fn summarise_cache(cache: &Cache) {
+    print!("\r");
+    for i in 1..=26 {
+        print!("{:02} ", cache.get(i).map(|x| x.len()).unwrap_or_default());
+    }
+}
 impl ButtonPad {
     fn from_str(input: &str) -> Self {
         let grid = Grid2d::from_str_as_char(input);
         let grid = grid.flip_y();
-        let position = grid.find(|&x| x == 'A').unwrap().0;
-        Self { grid, position }
+        Self { grid }
     }
-    fn press(&mut self, c: char) -> String {
-        //need to press c, we're at `self.position`.
-        let c_pos = self.grid.find(|&x| x == c).unwrap().0;
-        let (path, _cost) = pathfinding::directed::astar::astar_bag(
-            &self.position,
-            |&p| {
-                DIR_PRIORITY.iter()
-                .map(move |d| p.step(*d))
-                .filter(|&p| self.grid.get(p).is_some())
-                .filter(|&x| self.grid[x] != ' ')
-                .map(|x| (x, 1))
-            },
-            |x| x.manhattan_unsigned(&c_pos),
-            |x| *x == c_pos,
-        ).expect("no path");
-
-        let mut all_ans = Vec::new();
-        for s in path {
-            let mut ans: String = s
-                .into_iter()
-                .tuple_windows()
-                .map(|(a, b)| {
-                    let d = b.as_i().unwrap() - a.as_i().unwrap();
-                    Dir::try_from(d).unwrap().to_x(['^', 'v', '<', '>'])
-                })
-                .collect();
-            self.position = c_pos;
-            ans.push('A');
-            all_ans.push((ARROW_PAD.estimate_cost(&ans), ans));
+    fn lookup(&self, c: char) -> Point<usize> {
+        self.grid.find(|x| x == &c).unwrap().0
+    }
+    
+    fn type_string(&self, cache: &mut Cache, str: &str, robot_count: usize) -> usize {
+        let mut last_char = 'A';
+        let mut ans_cost = 0;
+        for c in str.chars() {
+            let cost = self.type_char(cache, last_char, c, robot_count);
+            ans_cost += cost;
+            last_char = c;
         }
-        all_ans.sort();
-        // println!("Paths from {:?} to {c_pos:?} is with cost {_cost}:", self.position);
-        // for (cost, a) in &all_ans {
-        //     println!("  {a} - {cost}");
-        // }
+        ans_cost
+    }
+    #[allow(dead_code)]
+    fn evaluate(&self, input: &str, with_spaces: bool) -> String {
+        let mut pos = self.lookup('A');
+        let mut ans = String::new();
+        for c in input.chars() {
+            let x = match c {
+                '<' => (' ', pos.left()),
+                '>' => (' ', pos.right()),
+                '^' => (' ', pos.up()),
+                'v' => (' ', pos.down()),
+                'A' => (self.grid[pos], pos),
+                ' ' => (' ', pos),
+                _ => unreachable!(),
+            };
+            pos = x.1;
+            if with_spaces || x.0 != ' ' {
+                ans.push(x.0);
+            }
+            assert!(self.grid.get(pos).is_some());
+            assert!(self.grid[pos] != ' ');
+        }
+        assert!(self.grid[pos] == 'A');
+        ans
+    }
+    fn type_char(&self, cache: &mut Cache, from: char, c: char, robot_count: usize) -> usize {
+        if let Some(a) = cache.get(robot_count).and_then(|m| m.get(&(from,c))) {
+            return *a;
+        }
+        if robot_count == 0 {
+            //this is a human. they can just type.
+            return 1;
+        }
+        if from == c {
+            //is same character. can just hit 'A' all the way up.
+            return 1;
+        }
+        // println!("Trying to press {c} from {from} at level {robot_count}");
+        // need path to get next robot to move me from from to c.
+        // next robot starts at A, needs to finish at 'A' and then press. 
+        let (_path, cost) = pathfinding::directed::dijkstra::dijkstra(
+            &(from, 'A'),
+            |(prev_button, prev_dir)| {
+                const DIRS: [char; 4] = ['<', 'v', '>', '^'];
+                let p = self.lookup(*prev_button);
+                let foo: Vec<_> = DIRS
+                    .iter()
+                    .filter_map(move |d| {
+                        let x = self
+                            .grid
+                            .get(p.step(Dir::from_x("^v<>", *d)))
+                            .filter(|p| **p != ' ');
+                        x.map(|c| (d, c))
+                    })
+                    .map(|(dir, new_sym)| {
+                        let mut cost = ARROW_PAD.type_char(cache, *prev_dir, *dir, robot_count - 1);
+                        if *new_sym == c {
+                            //this is the target. add on the keypress cost.
+                            let  p_cost = ARROW_PAD.type_char(cache, *dir, 'A',robot_count-1);
+                            cost += p_cost;
+                        }
+                        ((*new_sym, *dir), cost)
+                    })
+                    .collect();
+                foo
+            },
+            |(ch, _)| *ch == c ,
+        )
+        .expect("No path found");
 
-        all_ans.into_iter().min().unwrap().1
+        cache.entry(robot_count).or_default().insert((from,c), cost);
+        // summarise_cache(cache);
+        cost
     }
-    fn estimate_cost(&self, input: &str) -> usize {
-        // input.chars().fold(('A',0), |(a,cost),b|{
-        //     let pos_a = self.grid.find(|x| *x == a).unwrap().0;
-        //     let pos_b = self.grid.find(|x| *x == b).unwrap().0;
-        //     let cost = cost + pos_a.manhattan_unsigned(&pos_b);
-        //     (b, cost)
-        // }).1
-        input.chars().tuple_windows().map(|(a,b)| usize::from(a != b)).sum()
-    }
-    fn press_sequence(&mut self, s: &str) -> String {
-        // println!("\n\nTyping out {s}");
-        s.chars().map(|x| self.press(x)).collect()
-    }
+
 }
-// 212128 is too high
 
 lazy_static! {
     ///+---+---+---+
@@ -101,8 +147,9 @@ lazy_static! {
     static ref ARROW_PAD : ButtonPad = ButtonPad::from_str(" ^A\n<v>");
 }
 
+#[allow(dead_code)]
 fn print_ans(a: &[String], target: &str) {
-    let mut a_pos : Vec<usize> = Vec::new();
+    let mut a_pos: Vec<usize> = Vec::new();
     for l in a.iter().rev().map(|x| &x[..]).chain([target]) {
         let mut next_a = a_pos.into_iter();
         let mut new_as = Vec::new();
@@ -112,7 +159,7 @@ fn print_ans(a: &[String], target: &str) {
             let c = cs.next().unwrap();
             while pos < a {
                 print!(" ");
-                pos +=1;
+                pos += 1;
             }
             print!("{c}");
             if c == 'A' {
@@ -134,59 +181,29 @@ fn print_ans(a: &[String], target: &str) {
     println!();
 }
 
-fn p1(input: &str) -> usize {
-    let mut machines = [NUM_PAD.clone(), ARROW_PAD.clone(), ARROW_PAD.clone()];
+#[allow(dead_code)]
+fn breakdown(line: &str, depth: usize) {
+    let mut foo = line.to_string();
+    println!("{foo}");
+    for l in (0..depth).rev() {
+        if l > 0 {
+            foo = ARROW_PAD.evaluate(&foo, true);
+        } else {
+            foo = NUM_PAD.evaluate(&foo, true);
+        }
+        println!("{foo}");
+    }
+}
+fn p1<const ROBOTS: usize>(input: &str) -> usize {
     let mut ans = 0;
+    let mut cache = Cache::default();
     for l in input.lines() {
-        let seq : Vec<String> = machines
-            .iter_mut()
-            .scan(l.to_owned(), |acc, m|{*acc = m.press_sequence(&acc); Some(acc.clone())}).collect();
+        let x = NUM_PAD.type_string(&mut cache, l, ROBOTS);
         let num: usize = l[0..l.len() - 1].parse().unwrap();
-        println!("{l}:");
-        print_ans(&seq, l);
-        // println!("ans is {seq} * {num}");
-        ans += seq.last().unwrap().len() * num;
+        ans += x * num;
     }
     ans
 }
-
-//v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>A<vA>^AA<A>A<vA<A>>^AAA<A>vA^A
-//v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>AA<A>Av<A<A>>^AAA<A>vA^A
-
-// Correct for 379A:
-// <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-//    <   A > A  v <<   AA >  ^ AA > A  v  AA ^ A   < v  AAA >  ^ A
-// <A>Av<<AA>^AA>AvAA^A<vAAA>^A
-//        ^   A         <<      ^^   A     >>   A        vvv      A
-// ^A<<^^A>>AvvvA
-//            3                      7          9                 A
-// 379A
-
-// Me for 379A:
-// v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>AA<A>Av<A<A>>^AAA<A>vA^A
-//    <   A > A   <   AA  v <   AA >>  ^ A  v  AA ^ A  v <   AAA ^  > A
-// <A>A<AAv<AA>>^AvAA^Av<AAA^>Agg
-//  ^ A ^^  <<   A >> A  vvv  A
-// ^A^^<<A>>AvvvA
-// 379A
-
-// v<<A>>^AvA^Av<<A>>^AA<vA<A>>^AAvAA<^A>A<vA^>AA<A>Av<<A>A>^AAA<A>vA^A
-// <A>A<AAv<AA>>^AvAA^A<vAAA^>A
-// ^A^^<<A>>AvvvA
-// 379A
-
-
-// Correct for 029A
-// <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-//   v <<   A >>  ^ A   <   A > A  v  A   <  ^ AA > A   < v  AAA >  ^ A
-//          <       A       ^   A     >        ^^   A        vvv      A
-//                  0           2                   9                 A
-
-// Me for 029A
-// v<A<AA>^>AvA<^Av>A^Av<<A>^>AvA^Av<<A>^>AAv<A>A^A<A>Av<A<A>^>AAA<Av>A^A * 29   
-// v<<A>^>A<A>A<AAv>A^Av<AAA^>A                                                                                                          ▐
-// <A^A^^>AvvvA                                                                                                                          ▐
-// 029A                                                                                                                                  ▐
 
 const EG: &str = "029A
 980A
@@ -194,25 +211,3 @@ const EG: &str = "029A
 456A
 379A
 ";
-
-
-#[cfg(test)]
-mod test {
-    const EG1: &str = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-v<<A>>^A<A>AvA<^AA>A<vAAA>^A
-<A^A>^^AvvvA
-029A";
-    #[test]
-    fn example() {
-        let lines: Vec<&str> = EG1.lines().collect();
-        let mut my_num = super::NUM_PAD.clone();
-        let a: String = my_num.press_sequence(lines[3]);
-        assert_eq!(a.len(), lines[2].len());
-        println!("****");
-        let mut my_arr1 = super::ARROW_PAD.clone();
-        assert_eq!(my_arr1.press_sequence(lines[2]).len(), lines[1].len());
-        println!("****");
-        let mut my_arr2 = super::ARROW_PAD.clone();
-        assert_eq!(my_arr2.press_sequence(lines[1]).len(), lines[0].len());
-    }
-}
