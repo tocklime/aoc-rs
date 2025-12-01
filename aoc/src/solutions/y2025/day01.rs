c2acfada7cbeb86b0a89e1f85195cee6ef4dd88d
Greg Manning <greg@gregmanning.uk>
2025-12-01 15:47:02 +0000

    add answers for 2025d1

--- aoc/src/solutions/y2025/day01.rs
+++ aoc/src/solutions/y2025/day01.rs
@@ -1,31 +1,37 @@
 use nom::{
+    Parser,
     character::complete::{self, newline, one_of},
     combinator::all_consuming,
     multi::separated_list1,
-    Parser,
 };
 use num_traits::Euclid;
 use utils::nom::NomError;
 
-aoc_harness::aoc_main!(2025 day 1, generator gen_, part1 [p1], part2 [p2_slow, p2], example part1 EG => 3, example part2 EG => 6, example part2 EG2 => 4);
+aoc_harness::aoc_main!(2025 day 1, generator gen_, part1 [p1] => 1147, part2 [p2_slow, p2] => 6789, example part1 EG => 3, example part2 EG => 6, example part2 EG2 => 4);
 
-fn gen_(input: &str)-> Vec<i64> {
+fn gen_(input: &str) -> Vec<i64> {
     all_consuming::<_, NomError, _>(separated_list1(
         newline,
         (
             one_of("LR").map(|c| if c == 'L' { -1 } else { 1 }),
             complete::i64,
-        ).map(|(a,b)| a*b),
+        )
+            .map(|(a, b)| a * b),
     ))
-    .parse(input.trim_end()).unwrap().1
+    .parse(input.trim_end())
+    .unwrap()
+    .1
 }
 
 fn p1(ns: &[i64]) -> usize {
-    ns.iter().scan(50,|state, dir| {
-        *state += dir;
-        *state %= 100;
-        Some(*state)
-    }).filter(|&x| x == 0).count()
+    ns.iter()
+        .scan(50, |state, dir| {
+            *state += dir;
+            *state %= 100;
+            Some(*state)
+        })
+        .filter(|&x| x == 0)
+        .count()
 }
 fn turn_dial_slow(mut state: i64, direction: i64) -> (usize, i64) {
     let mut wraps = 0;
@@ -34,7 +40,7 @@ fn turn_dial_slow(mut state: i64, direction: i64) -> (usize, i64) {
         state += sign;
         state = state.rem_euclid(100);
         if state == 0 {
-            wraps +=1;
+            wraps += 1;
         }
     }
     (wraps, state)
@@ -56,19 +62,23 @@ fn turn_dial(state: i64, direction: i64) -> (usize, i64) {
     }
     (wraps, remmed)
 }
-fn p2_slow(ns:&[i64]) -> usize {
-    ns.iter().scan(50,|state, &dir| {
-        let (wraps, new_s) = turn_dial_slow(*state, dir);
-        *state = new_s;
-        Some(wraps)
-    }).sum()
+fn p2_slow(ns: &[i64]) -> usize {
+    ns.iter()
+        .scan(50, |state, &dir| {
+            let (wraps, new_s) = turn_dial_slow(*state, dir);
+            *state = new_s;
+            Some(wraps)
+        })
+        .sum()
 }
-fn p2(ns:&[i64]) -> usize {
-    ns.iter().scan(50,|state, &dir| {
-        let (wraps, new_s) = turn_dial(*state, dir);
-        *state = new_s;
-        Some(wraps)
-    }).sum()
+fn p2(ns: &[i64]) -> usize {
+    ns.iter()
+        .scan(50, |state, &dir| {
+            let (wraps, new_s) = turn_dial(*state, dir);
+            *state = new_s;
+            Some(wraps)
+        })
+        .sum()
 }
 
 const EG: &str = "L68
@@ -85,12 +95,12 @@ const EG2: &str = "R41\nL391";
 
 #[cfg(test)]
 mod test {
-    use proptest::proptest;
     use super::*;
-    proptest!{
+    use proptest::proptest;
+    proptest! {
         #[test]
         fn smart_is_equiv_to_slow(dial in 0..100i64, direction in -1000..1000i64) {
-            assert_eq!(turn_dial(dial, direction), turn_dial_slow(dial, direction))
+            assert_eq!(turn_dial(dial, direction), turn_dial_slow(dial, direction));
         }
 
     }
